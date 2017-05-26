77b30da1bdf40b292567fc0bf42fa119d9be5fd2
UMADEVI KAPU <kapu.umadevi@tcs.com>
2017-05-26 12:16:03 +0530

    [DURACI-6683] Implemented RMQConsumer Class
    Change-Id: I9d66a236a92571d2cca9cc6795b536d158b595d3

---
+++ src/main/java/com/ericsson/ei/rmq/consumer/RmqConsumer.java
@@ -0,0 +1,141 @@
+package com.ericsson.ei.rmq.consumer;
+
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class RmqConsumer {
+
+    @Value("${rabbitmq.queue.durable}") private Boolean queueDurable;
+    @Value("${rabbitmq.host}") private String host;
+    @Value("${rabbitmq.exchange.name}") private String exchangeName;
+    @Value("${rabbitmq.port}") private Integer port;
+    @Value("${rabbitmq.tls}") private String tlsVer;
+    @Value("${rabbitmq.user}") private String user;
+    @Value("${rabbitmq.password}") private String password;
+    @Value("${rabbitmq.domainId}") private String domainId;
+    @Value("${rabbitmq.componentName}") private String componentName;
+    @Value("${rabbitmq.routing.key}") private String routingKey;
+    @Value("${rabbitmq.consumerName}") private String consumerName;
+
+    SimpleMessageListenerContainer container;
+    ConnectionFactory factory;
+
+    public Boolean getQueueDurable() {
+        return queueDurable;
+    }
+
+    public void setQueueDurable(Boolean queueDurable) {
+        this.queueDurable = queueDurable;
+    }
+
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    public String getExchangeName() {
+        return exchangeName;
+    }
+
+    public void setExchangeName(String exchangeName) {
+        this.exchangeName = exchangeName;
+    }
+
+    public Integer getPort() {
+        return port;
+    }
+
+    public void setPort(Integer port) {
+        this.port = port;
+    }
+
+    public String getTlsVer() {
+        return tlsVer;
+    }
+
+    public void setTlsVer(String tlsVer) {
+        this.tlsVer = tlsVer;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getDomainId() {
+        return domainId;
+    }
+
+    public void setDomainId(String domainId) {
+        this.domainId = domainId;
+    }
+
+    public String getComponentName() {
+        return componentName;
+    }
+
+    public void setComponentName(String componentName) {
+        this.componentName = componentName;
+    }
+
+    public String getRoutingKey() {
+        return routingKey;
+    }
+
+    public void setRoutingKey(String routingKey) {
+        this.routingKey = routingKey;
+    }
+
+    public String getConsumerName() {
+        return consumerName;
+    }
+
+    public void setConsumerName(String consumerName) {
+        this.consumerName = consumerName;
+    }
+
+    @Bean
+    ConnectionFactory bindToMessageBus() {
+        factory = new CachingConnectionFactory(host,port);
+        return factory;
+    }
+
+    @Bean
+    SimpleMessageListenerContainer bindToQueueForRecentEvents(ConnectionFactory factory, EventHandler eventHandler) {
+        String durableName = queueDurable ? "durable" : "transient";
+        String queueName = domainId + "." + componentName + "." + consumerName + "." + durableName;
+        Queue queue = new Queue(queueName, queueDurable);
+        TopicExchange topicExchange = new TopicExchange(exchangeName);
+        BindingBuilder.bind(queue).to(topicExchange).with(routingKey);
+        MessageListenerAdapter listenerAdapter = new MessageListenerAdapter(eventHandler, "eventReceived");
+        container = new SimpleMessageListenerContainer();
+        container.setConnectionFactory(factory);
+        container.setQueueNames(queueName);
+        container.setMessageListener(listenerAdapter);
+        return container;
+    }
+}
---
+++ src/test/java/com/ericsson/ei/rmq/consumer/RmqConsumerTest.java
@@ -0,0 +1,100 @@
+package com.ericsson.ei.rmq.consumer;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
+public class RmqConsumerTest {
+
+    @Autowired
+    EventHandler eventHandler;
+
+    private Boolean queueDurable = true;
+    private String host = "127.0.0.1";
+    private String exchangeName = "ei-poc-4";
+    private Integer port = 5672;
+    private String domainId = "EN1";
+    private String componentName = "eiffelintelligence";
+    private String routingKey = "#";
+    private String consumerName = "messageConsumer";
+
+    @InjectMocks
+    RmqConsumer rmqConsumer;
+
+    @Mock ConnectionFactory factory;
+
+    @Before public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        initProperties();
+    }
+
+    public void initProperties()
+    {
+        rmqConsumer.setQueueDurable(queueDurable);
+        rmqConsumer.setHost(host);
+        rmqConsumer.setExchangeName(exchangeName);
+        rmqConsumer.setPort(port);
+        rmqConsumer.setDomainId(domainId);
+        rmqConsumer.setComponentName(componentName);
+        rmqConsumer.setRoutingKey(routingKey);
+        rmqConsumer.setConsumerName(consumerName);
+    }
+
+    @Test
+    public void getQueueDurableTest() {
+        assertTrue(rmqConsumer.getQueueDurable().equals(queueDurable));
+    }
+
+    @Test
+    public void getHostTest() {
+        assertTrue(rmqConsumer.getHost().equals(host));
+    }
+
+    @Test
+    public void getExchangeNameTest() {
+        assertTrue(rmqConsumer.getExchangeName().equals(exchangeName));
+    }
+
+    @Test
+    public void getPortTest() {
+        assertTrue(rmqConsumer.getPort().equals(port));
+    }
+
+    @Test
+    public void getDomainIdTest() {
+        assertTrue(rmqConsumer.getDomainId().equals(domainId));
+    }
+
+    @Test
+    public void getComponentNameTest() {
+        assertTrue(rmqConsumer.getComponentName().equals(componentName));
+    }
+
+    @Test
+    public void getRoutingKeyTest() {
+        assertTrue(rmqConsumer.getRoutingKey().equals(routingKey));
+    }
+
+    @Test
+    public void getConsumerNameTest() {
+        assertTrue(rmqConsumer.getConsumerName().equals(consumerName));
+    }
+
+    @Test
+    public void testMessageBusConnection() {
+        factory = rmqConsumer.bindToMessageBus();
+        assertNotNull(factory);
+    }
+}

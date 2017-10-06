899368a3545db0cc8c6a7ffd5bef6d3205b9b24e
Your Name <valentinkohd@gmail.com>
2017-10-06 15:05:39 +0300

    Test for measure performance EI is added (TrafficGeneratedTest). Number of packages of events (7 events in package) you can add in variable EVENT_PACKAGES. ALL, DEBUG, ERROR, FATAL, INFO, TRACE, WARN parametres are added to a jar file at the time of execution for start EI with logs. No parameters start EI without logs. /// Ex. with logs: java -jar /path/file.jar INFO Ex. without logs: java -jar /path/file.jar /// File ArtifactRules_new.json is modified - added posibility to send SourceChangeCreatedEvent and SourceChangeSubmittedEvent without error.

--- src/main/java/com/ericsson/ei/App.java
+++ src/main/java/com/ericsson/ei/App.java
@@ -5,12 +5,30 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 @SpringBootApplication
 @EnableAsync
 @EnableScheduling
 public class App {
 
     public static void main(String[] args) {
+
+        List<String> logLevels = new ArrayList<>();
+        Collections.addAll(logLevels, "ALL", "DEBUG", "ERROR", "FATAL", "INFO", "TRACE", "WARN");
+
+        if(logLevels.contains(args[0])) {
+            System.setProperty("logging.level.root", args[0]);
+            System.setProperty("logging.level.org.springframework.web", args[0]);
+            System.setProperty("logging.level.com.ericsson.ei", args[0]);
+        } else {
+            System.setProperty("logging.level.root", "OFF");
+            System.setProperty("logging.level.org.springframework.web", "OFF");
+            System.setProperty("logging.level.com.ericsson.ei", "OFF");
+        }
+
         SpringApplication.run(App.class, args);
     }
 

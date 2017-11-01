1a5ccce25ae7ff784e7399a1be798705626f43c1
ned29 <ned29051995>
2017-11-01 11:58:11 +0200

    Test

--- src/main/java/com/ericsson/ei/mongodbhandler/MongoDBHandler.java
+++ src/main/java/com/ericsson/ei/mongodbhandler/MongoDBHandler.java
@@ -16,23 +16,16 @@
 */
 package com.ericsson.ei.mongodbhandler;
 
-import java.util.ArrayList;
-
-import javax.annotation.PostConstruct;
-
+import com.mongodb.*;
+import com.mongodb.util.JSON;
+import lombok.Getter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import com.mongodb.BasicDBObject;
-import com.mongodb.DB;
-import com.mongodb.DBCollection;
-import com.mongodb.DBCursor;
-import com.mongodb.DBObject;
-import com.mongodb.MongoClient;
-import com.mongodb.WriteResult;
-import com.mongodb.util.JSON;
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
 
 @Component
 public class MongoDBHandler {
@@ -40,26 +33,31 @@ public class MongoDBHandler {
 
     MongoClient mongoClient;
 
+    @Getter
+    @Value("${mongodb.host}")
+    private String host;
+    @Getter
+    @Value("${mongodb.port}")
+    private int port;
+
     public void setMongoClient(MongoClient mongoClient) {
         this.mongoClient = mongoClient;
     }
 
-    @Value("${mongodb.host}") private String host;
-    @Value("${mongodb.port}") private int port;
-
     //TODO establish connection automatically when Spring instantiate this
     // based on connection data in properties file
-    @PostConstruct public void init() {
+    @PostConstruct
+    public void init() {
         createConnection(host, port);
     }
 
     //Establishing the connection to mongodb and creating a collection
-    public  void createConnection(String host, int port){
-        mongoClient = new MongoClient(host , port);
+    public void createConnection(String host, int port) {
+        mongoClient = new MongoClient(host, port);
     }
 
     //Insert data into collection
-    public  boolean insertDocument(String dataBaseName, String collectionName, String input){
+    public boolean insertDocument(String dataBaseName, String collectionName, String input) {
         try {
             DB db = mongoClient.getDB(dataBaseName);
             DBCollection table = db.getCollection(collectionName);
@@ -72,59 +70,57 @@ public class MongoDBHandler {
                 return result.wasAcknowledged();
             }
         } catch (Exception e) {
-            log.info(e.getMessage(),e);
+            log.info(e.getMessage(), e);
         }
         return false;
     }
 
     //Retrieve entire data from  collection
-    public ArrayList<String> getAllDocuments(String dataBaseName, String collectionName){
+    public ArrayList<String> getAllDocuments(String dataBaseName, String collectionName) {
         ArrayList<String> result = new ArrayList<>();
-        try{
+        try {
             DB db = mongoClient.getDB(dataBaseName);
             DBCollection table = db.getCollection(collectionName);
             DBCursor cursor = table.find();
-            if (cursor.count()!=0){
+            if (cursor.count() != 0) {
                 int i = 1;
                 while (cursor.hasNext()) {
                     DBObject document = cursor.next();
                     String documentStr = document.toString();
-                    log.info("Got Document: "+i);
+                    log.info("Got Document: " + i);
                     log.info(documentStr);
                     result.add(documentStr);
                     i++;
                 }
-            }
-            else{
+            } else {
                 log.info("No documents found in database: " + dataBaseName + "and collection: " + collectionName);
             }
-        }catch (Exception e) {
+        } catch (Exception e) {
             log.info(e.getMessage(), e);
         }
         return result;
     }
 
     //Retrieve data from the collection based on condition
-    public  ArrayList<String> find(String dataBaseName, String collectionName, String condition){
+    public ArrayList<String> find(String dataBaseName, String collectionName, String condition) {
         ArrayList<String> result = new ArrayList<>();
-        try{
+        try {
             DB db = mongoClient.getDB(dataBaseName);
             DBCollection table = db.getCollection(collectionName);
-            DBObject dbObjectCondition = (DBObject)JSON.parse(condition);
+            DBObject dbObjectCondition = (DBObject) JSON.parse(condition);
             DBCursor conditionalCursor = table.find(dbObjectCondition);
-            if (conditionalCursor.count()!=0){
-                while(conditionalCursor.hasNext()) {
+            if (conditionalCursor.count() != 0) {
+                while (conditionalCursor.hasNext()) {
                     DBObject object = conditionalCursor.next();
                     String documentStr = object.toString();
                     log.info(documentStr);
                     result.add(documentStr);
                 }
-            }
-            else{
+            } else {
                 log.info("No documents found with given condition: " + condition);
                 log.info("in database: " + dataBaseName + " and collection: " + collectionName);
             }
-        }catch (Exception e) {
+        } catch (Exception e) {
             log.info(e.getMessage(), e);
         }
         return result;
@@ -132,15 +128,15 @@ public class MongoDBHandler {
 
     //update the document in collection and remove the lock in one query. Lock is needed for multi process execution.
     //updateInput is updated document without lock
-    public  boolean updateDocument(String dataBaseName, String collectionName, String input, String updateInput ){
-        try{
+    public boolean updateDocument(String dataBaseName, String collectionName, String input, String updateInput) {
+        try {
             DB db = mongoClient.getDB(dataBaseName);
             DBCollection table = db.getCollection(collectionName);
-            DBObject dbObjectInput = (DBObject)JSON.parse(input);
-            DBObject dbObjectUpdateInput = (DBObject)JSON.parse(updateInput);
-            WriteResult result = table.update(dbObjectInput , dbObjectUpdateInput);
+            DBObject dbObjectInput = (DBObject) JSON.parse(input);
+            DBObject dbObjectUpdateInput = (DBObject) JSON.parse(updateInput);
+            WriteResult result = table.update(dbObjectInput, dbObjectUpdateInput);
             return result.isUpdateOfExisting();
-        }catch (Exception e) {
+        } catch (Exception e) {
             log.info(e.getMessage(), e);
         }
         return false;
@@ -148,45 +144,46 @@ public class MongoDBHandler {
 
     //Lock and return the document that matches the input condition in one query.
     //Lock is needed for multi process execution. This method is executed in a loop.
-    public  DBObject findAndModify(String dataBaseName, String collectionName, String input, String updateInput){
-        try{
+    public DBObject findAndModify(String dataBaseName, String collectionName, String input, String updateInput) {
+        try {
             DB db = mongoClient.getDB(dataBaseName);
             DBCollection table = db.getCollection(collectionName);
-            DBObject dbObjectInput = (DBObject)JSON.parse(input);
-            DBObject dbObjectUpdateInput = (DBObject)JSON.parse(updateInput);
-            DBObject result = table.findAndModify(dbObjectInput , dbObjectUpdateInput);
-            if (result != null){return result;}
-        }catch (Exception e) {
+            DBObject dbObjectInput = (DBObject) JSON.parse(input);
+            DBObject dbObjectUpdateInput = (DBObject) JSON.parse(updateInput);
+            DBObject result = table.findAndModify(dbObjectInput, dbObjectUpdateInput);
+            if (result != null) {
+                return result;
+            }
+        } catch (Exception e) {
             log.info(e.getMessage(), e);
         }
         return null;
     }
 
     //drop the document in collection
-    public  boolean dropDocument(String dataBaseName, String collectionName,String condition){
-        try{
+    public boolean dropDocument(String dataBaseName, String collectionName, String condition) {
+        try {
             DB db = mongoClient.getDB(dataBaseName);
             DBCollection table = db.getCollection(collectionName);
-            DBObject dbObjectCondition = (DBObject)JSON.parse(condition);
+            DBObject dbObjectCondition = (DBObject) JSON.parse(condition);
             WriteResult result = table.remove(dbObjectCondition);
-            if(result.getN()>0){
+            if (result.getN() > 0) {
                 return true;
-            }
-            else{
+            } else {
                 log.info("No documents found to delete");
                 return false;
             }
-        }catch (Exception e) {
+        } catch (Exception e) {
             log.info(e.getMessage(), e);
         }
         return false;
     }
 
-    public void createTTLIndex(String dataBaseName, String collectionName,String fieldName,int ttlValue){
+    public void createTTLIndex(String dataBaseName, String collectionName, String fieldName, int ttlValue) {
         DB db = mongoClient.getDB(dataBaseName);
-        BasicDBObject ttlField=new BasicDBObject(fieldName,1);
-        BasicDBObject ttlTime=new BasicDBObject("expireAfterSeconds",ttlValue);
-        db.getCollection(collectionName).createIndex(ttlField,ttlTime);
+        BasicDBObject ttlField = new BasicDBObject(fieldName, 1);
+        BasicDBObject ttlTime = new BasicDBObject("expireAfterSeconds", ttlValue);
+        db.getCollection(collectionName).createIndex(ttlField, ttlTime);
     }
 
 }

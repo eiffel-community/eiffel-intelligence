f2a931c1de2c592d5fa74ad0c80b8118a1116a4d
ned29 <ned29051995>
2017-11-01 12:11:08 +0200

    Fixes

--- src/main/java/com/ericsson/ei/controller/info/InstanceInfo.java
+++ src/main/java/com/ericsson/ei/controller/info/InstanceInfo.java
@@ -1,3 +1,19 @@
+/*
+   Copyright 2017 Ericsson AB.
+   For a full list of individual contributors, please see the commit history.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
 package com.ericsson.ei.controller.info;
 
 import com.ericsson.ei.controller.model.ParseInstanceInfoEI;
@@ -6,7 +22,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,12 +34,13 @@ public class InstanceInfo {
     @Autowired
     private ParseInstanceInfoEI istanceInfo;
 
-    @RequestMapping(value = "/information", produces = MediaType.APPLICATION_JSON_VALUE)
+    @CrossOrigin
+    @RequestMapping(value = "/information", produces = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.GET)
     public String parseInfo() {
         try {
             return new ObjectMapper().writerWithDefaultPrettyPrinter().writeValueAsString(istanceInfo);
         } catch (Exception e) {
-            LOGGER.error("Serialization is failed " + e.getMessage());
+            LOGGER.error("Serialization has failed " + e.getMessage());
         }
         return null;
     }

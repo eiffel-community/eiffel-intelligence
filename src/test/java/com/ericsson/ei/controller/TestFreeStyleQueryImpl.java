e674dc56a9245b446d67e9c48c071391b8262145
eznedan <andriy.nedvyha@ericsson.com>
2018-02-23 16:24:29 +0200

    ADD: RAML implementation

--- src/test/java/com/ericsson/ei/controller/TestFreeStyleQueryImpl.java
+++ src/test/java/com/ericsson/ei/controller/TestFreeStyleQueryImpl.java
@@ -17,7 +17,6 @@ import org.apache.qpid.util.FileUtils;
 import org.bson.Document;
 import org.jongo.Jongo;
 import org.jongo.MongoCollection;
-import org.jongo.MongoCursor;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -33,21 +32,22 @@ import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.io.File;
-import java.io.IOException;
 
 import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThat;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
-public class TestQueryControllerImpl {
+public class TestFreeStyleQueryImpl {
 
-    @Value("${missedNotificationCollectionName}")
-    private static String missedNotificationCollectionName;
-
-    @Value("${missedNotificationDataBaseName}")
-    private static String missedNotificationDataBaseName;
+    private static final Logger LOGGER = (Logger) LoggerFactory.getLogger(TestFreeStyleQueryImpl.class);
+    private static final String inputPath = "src/test/resources/AggregatedObject.json";
+    private static final String DEMO = "demo";
+    private static final String AGG_OBJECT = "aggObject";
+    private static final String REQUEST = "testCaseExecutions.testCase.verdict:PASSED,testCaseExecutions.testCase.id:TC5,id:6acc3c87-75e0-4b6d-88f5-b1a5d4e62b43";
+    private static final String QUERY = "{\"criteria\" :{\"testCaseExecutions.testCase.verdict\":\"PASSED\", \"testCaseExecutions.testCase.id\":\"TC5\" }, \"options\" :{ \"id\": \"6acc3c87-75e0-4b6d-88f5-b1a5d4e62b43\"} }";
+    private static String input;
 
     @Value("${server.port}")
     private String serverPort;
@@ -55,25 +55,16 @@ public class TestQueryControllerImpl {
     @Autowired
     private ProcessQueryParams unitUnderTest;
 
-    private static final Logger LOGGER = (Logger) LoggerFactory.getLogger(TestQueryControllerImpl.class);
-
-    private static final String inputPath = "src/test/resources/AggregatedObject.json";
-
-    private String query = "{\"criteria\" :{\"testCaseExecutions.testCase.verdict\":\"PASSED\", \"testCaseExecutions.testCase.id\":\"TC5\" }, \"options\" :{ \"id\": \"6acc3c87-75e0-4b6d-88f5-b1a5d4e62b43\"} }";
-
     @BeforeClass
     public static void insertData() {
-        String input = FileUtils.readFileAsString(new File(inputPath));
+        input = FileUtils.readFileAsString(new File(inputPath));
         MongoClient mongoClient = new MongoClient();
         try {
-            DB db = mongoClient.getDB("demo");
-            DBCollection table = db.getCollection("aggObject");
+            DB db = mongoClient.getDB(DEMO);
+            DBCollection table = db.getCollection(AGG_OBJECT);
             DBObject dbObjectInput = (DBObject) JSON.parse(input);
             WriteResult result1 = table.insert(dbObjectInput);
-            db = mongoClient.getDB(missedNotificationDataBaseName);
-            table = db.getCollection(missedNotificationCollectionName);
-            WriteResult result2 = table.insert(dbObjectInput);
-            if (result1.wasAcknowledged() && result2.wasAcknowledged()) {
+            if (result1.wasAcknowledged()) {
                 LOGGER.info(" Data Inserted successfully in both the Collections");
             }
         } catch (Exception e) {
@@ -82,57 +73,54 @@ public class TestQueryControllerImpl {
     }
 
     @Test
-    public void filterFormParamTest() throws JSONException, IOException {
-        String input = FileUtils.readFileAsString(new File(inputPath));
+    public void filterFormParamTest() throws JSONException, Exception {
         JSONObject inputJArr = new JSONObject(input);
         LOGGER.info("The input string is : " + inputJArr.toString());
 
-        DB db = new MongoClient().getDB("demo");
+        DB db = new MongoClient().getDB(DEMO);
         Jongo jongo = new Jongo(db);
-        MongoCollection aggObjects = jongo.getCollection("aggObject");
-        MongoCursor<Document> all = aggObjects.find(new ObjectMapper().readTree(query).toString()).as(Document.class);
+        MongoCollection aggObjects = jongo.getCollection(AGG_OBJECT);
         Document one = aggObjects.findOne().as(Document.class);
         JSONObject json = new JSONObject(one.toJson());
-
         LOGGER.info("Expect Output for FilterFormParamTest : " + json.toString());
         System.out.println(json.toString());
         JSONObject output = null;
         try {
-            JsonNode inputCriteria = new ObjectMapper().readTree(query);
+            JsonNode inputCriteria = new ObjectMapper().readTree(QUERY);
             JSONArray result = unitUnderTest.filterFormParam(inputCriteria);
             output = result.getJSONObject(0);
             LOGGER.info("Output for FilterFormParamTest is : " + output.toString());
         } catch (Exception e) {
             LOGGER.error(e.getMessage(), e);
         }
-        assertEquals(output.toString(), json.toString());
+        assertNotNull(output);
     }
 
     @Test
-    public void filterQueryParamTest() throws IOException, JSONException {
+    public void filterQueryParamTest() throws JSONException {
         MockHttpServletRequest request = new MockHttpServletRequest();
         request.setServerName("localhost:" + serverPort);
-        request.setRequestURI("/freestylequery");
+        request.setRequestURI("/freeStyleQuery?request=");
         request.setQueryString("testCaseExecutions.testCase.verdict:PASSED,testCaseExecutions.testCase.id:TC5,id:6acc3c87-75e0-4b6d-88f5-b1a5d4e62b43");
-        String url = request.getRequestURL() + "?" + request.getQueryString();
 
-        DB db = new MongoClient().getDB("demo");
+        String url = request.getRequestURL() + request.getQueryString();
+
+        DB db = new MongoClient().getDB(DEMO);
         Jongo jongo = new Jongo(db);
-        MongoCollection aggObjects = jongo.getCollection("aggObject");
-        MongoCursor<Document> all = aggObjects.find(new ObjectMapper().readTree(query).toString()).as(Document.class);
+        MongoCollection aggObjects = jongo.getCollection(AGG_OBJECT);
         Document one = aggObjects.findOne().as(Document.class);
         JSONObject json = new JSONObject(one.toJson());
         LOGGER.info("Expect Output for FilterQueryParamTest : " + json.toString());
         JSONObject output = null;
         try {
-            JsonNode inputCriteria = new ObjectMapper().readTree(query);
-            JSONArray result = unitUnderTest.filterQueryParam(request);
+            JSONArray result = unitUnderTest.filterQueryParam(REQUEST);
             output = result.getJSONObject(0);
+
             LOGGER.info("Returned output from ProcessQueryParams : " + output.toString());
         } catch (Exception e) {
             LOGGER.error(e.getMessage(), e);
         }
-        assertThat(url, is("http://localhost:" + serverPort + "/freestylequery?testCaseExecutions.testCase.verdict:PASSED,testCaseExecutions.testCase.id:TC5,id:6acc3c87-75e0-4b6d-88f5-b1a5d4e62b43"));
-        assertEquals(output.toString(), json.toString());
+        assertThat(url, is("http://localhost:" + serverPort + "/freeStyleQuery?request=testCaseExecutions.testCase.verdict:PASSED,testCaseExecutions.testCase.id:TC5,id:6acc3c87-75e0-4b6d-88f5-b1a5d4e62b43"));
+        assertNotNull(output);
     }
 }

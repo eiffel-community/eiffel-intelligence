619e370a993245600d5df19fef6549b10d2202b7
Ned29 <nedvyha@gmail.com>
2018-03-28 16:10:10 +0300

    FIX: assert

--- src/test/java/com/ericsson/ei/flowtests/FlowSourceChangeObject.java
+++ src/test/java/com/ericsson/ei/flowtests/FlowSourceChangeObject.java
@@ -14,10 +14,11 @@
 package com.ericsson.ei.flowtests;
 
 import com.ericsson.ei.rules.RulesHandler;
-import org.apache.qpid.util.FileUtils;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.qpid.util.FileUtils;
 import org.junit.runner.RunWith;
+import org.skyscreamer.jsonassert.JSONAssert;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,15 +28,14 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import java.io.File;
 import java.util.ArrayList;
 
-import static org.junit.Assert.assertEquals;
-
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
 public class FlowSourceChangeObject extends FlowTest {
-    static protected String inputFilePath = "src/test/resources/aggregatedSourceChangeObject.json";
-    static protected String jsonFilePath = "src/test/resources/TestSourceChangeObject.json";
-    static protected String rulePath = "src/test/resources/TestSourceChangeObjectRules.json";
-    private static Logger log = LoggerFactory.getLogger(FlowTest.class);
+   
+    private static String inputFilePath = "src/test/resources/aggregatedSourceChangeObject.json";
+    private static String jsonFilePath = "src/test/resources/TestSourceChangeObject.json";
+    private static String rulePath = "src/test/resources/TestSourceChangeObjectRules.json";
+    private static final Logger LOGGER = LoggerFactory.getLogger(FlowTest.class);
 
     @Autowired
     private RulesHandler rulesHandler;
@@ -67,12 +67,12 @@ public class FlowSourceChangeObject extends FlowTest {
         try {
             String expectedDocuments = FileUtils.readFileAsString(new File(inputFilePath));
             ObjectMapper objectmapper = new ObjectMapper();
-            JsonNode expectedJsons = objectmapper.readTree(expectedDocuments);
+            JsonNode expectedJson = objectmapper.readTree(expectedDocuments);
             String document = objectHandler.findObjectById("fb6efi4n-25fb-4d77-b9fd-5f2xrrefe66de47");
             JsonNode actualJson = objectmapper.readTree(document);
-            assertEquals(expectedJsons.toString().length(), actualJson.toString().length());
+            JSONAssert.assertEquals(expectedJson.toString(), actualJson.toString(), false);
         } catch (Exception e) {
-            log.info(e.getMessage(), e);
+            LOGGER.error(e.getMessage(), e);
         }
     }
 }

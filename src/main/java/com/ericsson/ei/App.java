270a11767ae42c7f56796aad8a822551e02781b3
Raja Maragani <raja.maragani@tcs.com>
2018-04-03 09:39:03 +0200

    Rest end point for run rules on list events and find aggregated object (#68)
    * Implemented code for multiple users access EI finctionality,
    Implemented code for run rules on list events and find aggregated object
    
    * added test cases and aggregated enabled flag in prop file

--- src/main/java/com/ericsson/ei/App.java
+++ src/main/java/com/ericsson/ei/App.java
@@ -16,43 +16,51 @@
 */
 package com.ericsson.ei;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.beans.factory.config.Scope;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.support.SimpleThreadScope;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 
 @SpringBootApplication
 @EnableAsync
 @EnableScheduling
 public class App extends SpringBootServletInitializer {
 
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(App.class);
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(App.class);
+    }
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		List<String> logLevels = new ArrayList<>();
-		Collections.addAll(logLevels, "ALL", "DEBUG", "ERROR", "FATAL", "INFO", "TRACE", "WARN");
+        List<String> logLevels = new ArrayList<>();
+        Collections.addAll(logLevels, "ALL", "DEBUG", "ERROR", "FATAL", "INFO", "TRACE", "WARN");
 
-		if (args != null && args.length > 0 && logLevels.contains(args[0])) {
-			System.setProperty("logging.level.root", args[0]);
-			System.setProperty("logging.level.org.springframework.web", args[0]);
-			System.setProperty("logging.level.com.ericsson.ei", args[0]);
-		} else {
-			System.setProperty("logging.level.root", "OFF");
-			System.setProperty("logging.level.org.springframework.web", "OFF");
-			System.setProperty("logging.level.com.ericsson.ei", "OFF");
-		}
+        if (args != null && args.length > 0 && logLevels.contains(args[0])) {
+            System.setProperty("logging.level.root", args[0]);
+            System.setProperty("logging.level.org.springframework.web", args[0]);
+            System.setProperty("logging.level.com.ericsson.ei", args[0]);
+        } else {
+            System.setProperty("logging.level.root", "OFF");
+            System.setProperty("logging.level.org.springframework.web", "OFF");
+            System.setProperty("logging.level.com.ericsson.ei", "OFF");
+        }
 
-		SpringApplication.run(App.class, args);
-	}
+        ConfigurableBeanFactory beanFactory = new DefaultListableBeanFactory();
+        Scope threadScope = new SimpleThreadScope();
+        beanFactory.registerScope("thread", threadScope);
+
+        SpringApplication.run(App.class, args);
+    }
 
 }

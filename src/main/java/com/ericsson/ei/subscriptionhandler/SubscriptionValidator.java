b8e5b88b0186936a6510d30142bf512f0368d626
Mohammad Saifullah <33659596+sericcson@users.noreply.github.com>
2018-04-09 13:43:53 +0200

    Subscriptions need to handle multiple Mail receivers  (#82)
    * JMESPath REST API
    
    * JMESPath  API with fixed reviews
    
    * partial work jmespath rest
    
    * JMESPath API with RAML 0.8 specification
    
    * remove componet comment
    
    * replace tab with spaces
    
    * fix usage examples
    
    * prepare for new asserts in flow test
    
    * fix unit tests
    
    * Subscription for individual events
    
    * pom file
    
    * subscription individual objects
    
    * pom
    
    * error removal
    
    * Subscription Individual Object
    
    * deleting unused import
    
    * jmespath comment correction
    
    * space removal and new clean build
    
    * review changes
    
    * review change file deleted
    
    * some files
    
    * sendmail
    
    * dded some test code
    
    * deleting from other story
    
    * removig from other stories
    
    * raml
    
    * added missing property
    
    * added missing property
    
    * removed unrelated code
    
    * removing subscription rules
    
    * review
    
    * handler

--- src/main/java/com/ericsson/ei/subscriptionhandler/SubscriptionValidator.java
+++ src/main/java/com/ericsson/ei/subscriptionhandler/SubscriptionValidator.java
@@ -17,6 +17,7 @@
 package com.ericsson.ei.subscriptionhandler;
 
 import java.util.List;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.apache.commons.lang3.StringUtils;
@@ -29,135 +30,158 @@ import com.ericsson.ei.controller.model.NotificationMessageKeyValue;
 import org.springframework.http.MediaType;
 
 public class SubscriptionValidator {
-	
-	private static final Logger LOG = LoggerFactory.getLogger(SubscriptionValidator.class);
-	
-	/*
-	 * Validation of parameters values in subscriptions objects.
-	 * Throws SubscriptionValidationException if validation of a parameter fails due to wrong
-	 * format of parameter.
-	 * 
-	 * @param Subscription 
-	 */
-	public void validateSubscription(Subscription subscription) throws SubscriptionValidationException {
-		
-		LOG.info("Validation of subscription " + subscription.getSubscriptionName() + " Started.");
-		this.validateSubscriptionName(subscription.getSubscriptionName());
-		this.validateNotificationMessageKeyValues(subscription.getNotificationMessageKeyValues(), subscription.getRestPostBodyMediaType() );
-		this.validateNotificationMeta(subscription.getNotificationMeta());
-		this.validateNotificationType(subscription.getNotificationType());
-		if(subscription.getNotificationType().equals("REST_POST")) {
-			this.RestPostMediaType(subscription.getRestPostBodyMediaType());
-		}
-//		List<Requirement> reqList = subscription.getRequirements();
-//		for (int i=0; i < reqList.size(); i++) {
-//			this.validateJmespath(reqList.get(i).getConditions().get(0).getJmespath());
-//		}
-		LOG.info("Validating of subscription " + subscription.getSubscriptionName() + " finished successfully.");
-	}
-	
-	/*
-	 * Validation of subscriptionName parameter
-	 * Throws SubscriptionValidationException if validation of the parameter fails due to wrong
-	 * format of parameter.
-	 * 
-	 * @param subscriptionName
-	 */
-	public void validateSubscriptionName(String subscriptionName) throws SubscriptionValidationException {
-		
-		String regex = "^[A-Za-z0-9_]+$";
-		
-		if (!Pattern.matches(regex, subscriptionName)) {
-			throw new SubscriptionValidationException("Wrong format of SubscriptionName: " + subscriptionName);
-		}	
-	}
 
+    private static final Logger LOG = LoggerFactory.getLogger(SubscriptionValidator.class);
 
-	/*
-	 * Validation of NotificationMessageKeyValues parameters (key/values)
-	 * Throws SubscriptionValidationException if validation of the parameter fails due to wrong
-	 * format of parameter.
-	 *
-	 * @param notificationMessage
-	 * @param restPostBodyMediaType
-	 */
+    /*
+     * Validation of parameters values in subscriptions objects. Throws
+     * SubscriptionValidationException if validation of a parameter fails due to
+     * wrong format of parameter.
+     * 
+     * @param Subscription
+     */
+    public void validateSubscription(Subscription subscription) throws SubscriptionValidationException {
 
-	public void validateNotificationMessageKeyValues(List<NotificationMessageKeyValue> notificationMessage, String restPostBodyMediaType) throws SubscriptionValidationException {
+        LOG.info("Validation of subscription " + subscription.getSubscriptionName() + " Started.");
+        this.validateSubscriptionName(subscription.getSubscriptionName());
+        this.validateNotificationMessageKeyValues(subscription.getNotificationMessageKeyValues(),
+                subscription.getRestPostBodyMediaType());
+        this.validateNotificationMeta(subscription.getNotificationMeta());
+        this.validateNotificationType(subscription.getNotificationType());
+        if (subscription.getNotificationType().equals("REST_POST")) {
+            this.RestPostMediaType(subscription.getRestPostBodyMediaType());
+        }
+        // List<Requirement> reqList = subscription.getRequirements();
+        // for (int i=0; i < reqList.size(); i++) {
+        // this.validateJmespath(reqList.get(i).getConditions().get(0).getJmespath());
+        // }
+        LOG.info("Validating of subscription " + subscription.getSubscriptionName() + " finished successfully.");
+    }
 
-		for (NotificationMessageKeyValue item : notificationMessage) {
+    /*
+     * Validation of subscriptionName parameter Throws
+     * SubscriptionValidationException if validation of the parameter fails due to
+     * wrong format of parameter.
+     * 
+     * @param subscriptionName
+     */
+    public void validateSubscriptionName(String subscriptionName) throws SubscriptionValidationException {
 
-				String test_key = item.getFormkey();
-				String test_value = item.getFormvalue();
+        String regex = "^[A-Za-z0-9_]+$";
 
-			if (restPostBodyMediaType != null && restPostBodyMediaType.equals(MediaType.APPLICATION_FORM_URLENCODED.toString())) { // FORM/POST PARAMETERS
-               if((test_key == null || test_key.isEmpty() || StringUtils.isBlank(test_key)) || (test_value == null || test_value.isEmpty() || StringUtils.isBlank(test_value))){
-				   throw new SubscriptionValidationException("Value & Key  in notificationMessage must have a values: " + notificationMessage);
-			   }
+        if (!Pattern.matches(regex, subscriptionName)) {
+            throw new SubscriptionValidationException("Wrong format of SubscriptionName: " + subscriptionName);
+        }
+    }
 
-			} else {
-				if(notificationMessage.size() != 1 ){
-					throw new SubscriptionValidationException("Only one array is allowed for notificationMessage when NOT using key/value pairs: " + notificationMessage);
-				}
-				else if(test_key !=null && !test_key.isEmpty()){
-					throw new SubscriptionValidationException("Key in notificationMessage must be empty when NOT using key/value pairs: " + notificationMessage);
-				}
-				else if(StringUtils.isBlank(test_value)){
-					throw new SubscriptionValidationException("Value in notificationMessage must have a value when NOT using key/value pairs: " + notificationMessage);
-				}
+    /*
+     * Validation of NotificationMessageKeyValues parameters (key/values) Throws
+     * SubscriptionValidationException if validation of the parameter fails due to
+     * wrong format of parameter.
+     *
+     * @param notificationMessage
+     * 
+     * @param restPostBodyMediaType
+     */
 
+    public void validateNotificationMessageKeyValues(List<NotificationMessageKeyValue> notificationMessage,
+            String restPostBodyMediaType) throws SubscriptionValidationException {
 
-			}
-		}
+        for (NotificationMessageKeyValue item : notificationMessage) {
 
-	}
+            String test_key = item.getFormkey();
+            String test_value = item.getFormvalue();
 
+            if (restPostBodyMediaType != null
+                    && restPostBodyMediaType.equals(MediaType.APPLICATION_FORM_URLENCODED.toString())) { // FORM/POST
+                                                                                                         // PARAMETERS
+                if ((test_key == null || test_key.isEmpty() || StringUtils.isBlank(test_key))
+                        || (test_value == null || test_value.isEmpty() || StringUtils.isBlank(test_value))) {
+                    throw new SubscriptionValidationException(
+                            "Value & Key  in notificationMessage must have a values: " + notificationMessage);
+                }
 
-	/*
-	 * Validation of notificationMeta parameter
-	 * Throws SubscriptionValidationException if validation of the parameter fails due to wrong
-	 * format of parameter.
-	 * 
-	 * @param notificationMeta
-	 */
-	public void validateNotificationMeta(String notificationMeta) throws SubscriptionValidationException {
-		String regex = ".*[\\s].*";
-		
-		if (Pattern.matches(regex, notificationMeta)) {
-			throw new SubscriptionValidationException("Wrong format of NotificationMeta: " + notificationMeta);
-		}
-	}
-	
-	/*
-	 * Validation of notificationType parameter
-	 * Throws SubscriptionValidationException if validation of the parameter fails due to wrong
-	 * format of parameter.
-	 * 
-	 * @param notificationType
-	 */
-	public void validateNotificationType(String notificationType) throws SubscriptionValidationException {
-		String regexMail = "[\\s]*MAIL[\\\\s]*";
-		String regexRestPost = "[\\s]*REST_POST[\\\\s]*";
+            } else {
+                if (notificationMessage.size() != 1) {
+                    throw new SubscriptionValidationException(
+                            "Only one array is allowed for notificationMessage when NOT using key/value pairs: "
+                                    + notificationMessage);
+                } else if (test_key != null && !test_key.isEmpty()) {
+                    throw new SubscriptionValidationException(
+                            "Key in notificationMessage must be empty when NOT using key/value pairs: "
+                                    + notificationMessage);
+                } else if (StringUtils.isBlank(test_value)) {
+                    throw new SubscriptionValidationException(
+                            "Value in notificationMessage must have a value when NOT using key/value pairs: "
+                                    + notificationMessage);
+                }
 
-		if (!(Pattern.matches(regexMail, notificationType) || Pattern.matches(regexRestPost, notificationType))) {
-			throw new SubscriptionValidationException("Wrong format of NotificationType: " + notificationType);
-		}
-	}
+            }
+        }
 
+    }
 
-	public void RestPostMediaType(String restPostMediaType) throws SubscriptionValidationException {
-		String regexApplication_JSON = "[\\s]*application/json[\\\\s]*";
-		String regexApplicationFormUrlEncoded = "[\\s]*application/x-www-form-urlencoded[\\\\s]*";
+    /*
+     * Validation of notificationMeta parameter Throws
+     * SubscriptionValidationException if validation of the parameter fails due to
+     * wrong format of parameter.
+     * 
+     * @param notificationMeta
+     */
+    public void validateNotificationMeta(String notificationMeta) throws SubscriptionValidationException {
+        String regex = ".*[\\s].*";
 
-		if (!(Pattern.matches(regexApplication_JSON, restPostMediaType) || Pattern.matches(regexApplicationFormUrlEncoded, restPostMediaType))) {
-			throw new SubscriptionValidationException("Wrong format of RestPostMediaType: " + restPostMediaType);
-		}
-	}
+        if (Pattern.matches(regex, notificationMeta)) {
+            throw new SubscriptionValidationException("Wrong format of NotificationMeta: " + notificationMeta);
+        }
+    }
 
+    /*
+     * Validation of notificationType parameter Throws
+     * SubscriptionValidationException if validation of the parameter fails due to
+     * wrong format of parameter.
+     * 
+     * @param notificationType
+     */
+    public void validateNotificationType(String notificationType) throws SubscriptionValidationException {
+        String regexMail = "[\\s]*MAIL[\\\\s]*";
+        String regexRestPost = "[\\s]*REST_POST[\\\\s]*";
 
+        if (!(Pattern.matches(regexMail, notificationType) || Pattern.matches(regexRestPost, notificationType))) {
+            throw new SubscriptionValidationException("Wrong format of NotificationType: " + notificationType);
+        }
+    }
 
+    public void RestPostMediaType(String restPostMediaType) throws SubscriptionValidationException {
+        String regexApplication_JSON = "[\\s]*application/json[\\\\s]*";
+        String regexApplicationFormUrlEncoded = "[\\s]*application/x-www-form-urlencoded[\\\\s]*";
 
-//	public void validateJmespath(String jmespath) {
-////		TODO: Validator for Jmepath syntax need to be implemented here.
-//		throw new SubscriptionValidationException("Wrong format/syntax of Jmepath: " + jmespath);
-//	}
+        if (!(Pattern.matches(regexApplication_JSON, restPostMediaType)
+                || Pattern.matches(regexApplicationFormUrlEncoded, restPostMediaType))) {
+            throw new SubscriptionValidationException("Wrong format of RestPostMediaType: " + restPostMediaType);
+        }
+    }
+
+    /*
+     * Validation of email address Throws SubscriptionValidationException if
+     * validation of the parameter fails due to wrong format of parameter.
+     * 
+     * @param email
+     */
+    public void validateEmail(String email) throws SubscriptionValidationException {
+        final Pattern VALID_EMAIL_ADDRESS_REGEX = Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$",
+                Pattern.CASE_INSENSITIVE);
+        Matcher matcher = VALID_EMAIL_ADDRESS_REGEX.matcher(email);
+
+        if (!(matcher.matches())) {
+            throw new SubscriptionValidationException("Wrong email address: " + email);
+        }
+        ;
+    }
+
+    // public void validateJmespath(String jmespath) {
+    //// TODO: Validator for Jmepath syntax need to be implemented here.
+    // throw new SubscriptionValidationException("Wrong format/syntax of Jmepath: "
+    // + jmespath);
+    // }
 }

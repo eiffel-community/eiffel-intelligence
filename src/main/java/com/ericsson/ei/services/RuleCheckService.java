68ff28d15fa6ae3ffaf92012d05a82c733c1ae12
Valentin <valentin.tyhonov@gmail.com>
2018-04-09 14:31:16 +0300

    Fixed rule-check enpoint (#83)
    * Update fork (#6)
    
    * Fixed rule-check endpoints
    
    * Fixed rule-check endpoints
    
    * Fixed rule-check endpoints
    
    * Fixed error response
    
    * Fixed error response

--- src/main/java/com/ericsson/ei/services/RuleCheckService.java
+++ src/main/java/com/ericsson/ei/services/RuleCheckService.java
@@ -3,7 +3,6 @@ package com.ericsson.ei.services;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.Random;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -17,52 +16,47 @@ import com.ericsson.ei.handlers.EventHandler;
 import com.ericsson.ei.handlers.EventToObjectMapHandler;
 import com.ericsson.ei.jmespath.JmesPathInterface;
 import com.ericsson.ei.queryservice.ProcessAggregatedObject;
-import com.fasterxml.jackson.core.JsonProcessingException;
 
 @Component
 public class RuleCheckService implements IRuleCheckService {
+    private static final Logger LOGGER = LoggerFactory.getLogger(RuleCheckService.class);
 
     @Autowired
-    JmesPathInterface jmesPathInterface;
+    private JmesPathInterface jmesPathInterface;
 
     @Autowired
-    EventHandler eventHandler;
+    private EventHandler eventHandler;
 
     @Autowired
     private ProcessAggregatedObject processAggregatedObject;
 
     @Autowired
-    EventToObjectMapHandler eventToObjectMapHandler;
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(RuleCheckService.class);
+    private EventToObjectMapHandler eventToObjectMapHandler;
 
     @Override
-    public String prepareAggregatedObject(String listRulesJson, String listEventsJson)
-            throws JSONException, JsonProcessingException, IOException {
-        JSONArray rulesList = new JSONArray(listRulesJson);
-        JSONArray eventsList = new JSONArray(listEventsJson);
-        eventHandler.getRulesHandler().setParsedJson(rulesList.toString());
-        String response = null;
+    public String prepareAggregatedObject(JSONArray listRulesJson, JSONArray listEventsJson)
+            throws JSONException, IOException {
+        eventHandler.getRulesHandler().setParsedJson(listRulesJson.toString());
+        String response;
         // Looping all events and add suffix template name to id and links, For
         // identifying the test aggregated events.
         HashSet<String> templateNames = new HashSet<>();
-        for (int i = 0; i < eventsList.length(); i++) {
+        for (int i = 0; i < listEventsJson.length(); i++) {
             String templateName = jmesPathInterface
-                    .runRuleOnEvent("TemplateName", rulesList.getJSONObject(i).toString()).asText("TEST");
+                    .runRuleOnEvent("TemplateName", listRulesJson.getJSONObject(i).toString()).asText("TEST");
             templateNames.add(templateName);
             if (templateNames.size() == 1) {
-                addTemplateNameToIds(eventsList.getJSONObject(i), templateName);
-                LOGGER.debug("event to prepare aggregated object :: " + eventsList.getJSONObject(i).toString());
-                eventHandler.eventReceived(eventsList.getJSONObject(i).toString());
+                addTemplateNameToIds(listEventsJson.getJSONObject(i), templateName);
+                LOGGER.debug("event to prepare aggregated object :: " + listEventsJson.getJSONObject(i).toString());
+                eventHandler.eventReceived(listEventsJson.getJSONObject(i).toString());
             }
         }
-
         String templateName = templateNames.iterator().next();
         if (templateNames.size() == 1) {
             ArrayList<String> responseList = processAggregatedObject.getAggregatedObjectByTemplateName(templateName);
             response = responseList.toString();
         } else {
-            response = "Multipul template names are not allowed, Please use single name for all rules.";
+            response = "Multiple template names are not allowed, Please use single name for all rules.";
         }
         // Delete the aggregated object
         processAggregatedObject.deleteAggregatedObject(templateName);

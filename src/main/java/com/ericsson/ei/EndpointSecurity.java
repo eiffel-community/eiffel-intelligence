e35933b6ee033fa7dfd8dc8eb42c6e765f8c2bfc
Christoffer Cortes Sjöwall <christoffer.cortes.sjowall@ericsson.com>
2018-04-16 16:09:07 +0200

    Improvements to LDAP security

--- src/main/java/com/ericsson/ei/EndpointSecurity.java
+++ src/main/java/com/ericsson/ei/EndpointSecurity.java
@@ -1,9 +1,25 @@
-package com.ericsson.ei.config;
+/*
+   Copyright 2017 Ericsson AB.
+   For a full list of individual contributors, please see the commit history.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+package com.ericsson.ei;
 
 import org.apache.tomcat.util.codec.binary.Base64;
 import org.apache.tomcat.util.codec.binary.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -13,43 +29,52 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 
 @Configuration
-@PropertySource(value = "file:${catalina.home}/conf/security.properties", ignoreResourceNotFound = true)
-@ConditionalOnProperty(value = "ldap.enabled")
-@EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+@PropertySource(value = "file:/home/ezcorch/Public/security.properties", ignoreResourceNotFound = true)
 
-    @Value("${ldap.url}")
+@EnableWebSecurity
+public class EndpointSecurity extends WebSecurityConfigurerAdapter {
+    @Value("${ldap.enabled:false}")
+    private boolean ldapEnabled;
+    
+    @Value("${ldap.url:default}")
     private String ldapUrl;
     
-    @Value("${ldap.base.dn}")
+    @Value("${ldap.base.dn:default}")
     private String ldapBaseDn;
     
-    @Value("${ldap.username}")
+    @Value("${ldap.username:default}")
     private String ldapUsername;
     
-    @Value("${ldap.password}")
+    @Value("${ldap.password:default}")
     private String ldapPassword;
     
-    @Value("${ldap.user.filter}")
+    @Value("${ldap.user.filter:default}")
     private String ldapUserFilter;
     
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
+        if(ldapEnabled) {
+            http
             .authorizeRequests()
                 .anyRequest().authenticated()
             .and()
                 .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.ALWAYS)
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
             .and()
                 .httpBasic()
             .and()
                 .csrf().disable();
+        }
+        else {
+            http
+            .csrf().disable();
+        }
     }
 
     @Override
     public void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth
+        if(ldapEnabled) {
+            auth
             .eraseCredentials(false)
             .ldapAuthentication()
                 .userSearchFilter(ldapUserFilter)
@@ -58,6 +83,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                     .root(ldapBaseDn)
                     .managerDn(ldapUsername)
                     .managerPassword(decodeBase64(ldapPassword));
+        }
     }
     
     private String decodeBase64(String password) {

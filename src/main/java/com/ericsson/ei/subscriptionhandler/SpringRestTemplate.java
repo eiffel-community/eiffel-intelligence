50f1d46bf60d0d8782313bf4310544bc00fcb2c2
evasiba-ericsson <baluta.vasile@gmail.com>
2018-04-17 08:24:06 +0200

    rebase (#12)
    * Subscription handler function tests (#78)
    
    * ADD: code refactoring, delete unused values, change loggers to debug and ect.
    
    * ADD: integration test for subscription
    
    * fixes
    
    * FIX: mail trigger
    
    * CHANGE: LOG to LOGGER
    
    * FIX: review comments
    
    * FIX: review comments
    
    * fixes
    
    * fixes
    
    * FIX: review comments
    
    * fixes
    
    * Step version for release

--- src/main/java/com/ericsson/ei/subscriptionhandler/SpringRestTemplate.java
+++ src/main/java/com/ericsson/ei/subscriptionhandler/SpringRestTemplate.java
@@ -16,31 +16,24 @@
 */
 package com.ericsson.ei.subscriptionhandler;
 
-import com.ericsson.ei.jmespath.JmesPathInterface;
+import com.fasterxml.jackson.databind.JsonNode;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.*;
 import org.springframework.stereotype.Component;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestOperations;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import java.util.List;
-
 /**
  * This class is responsible to send notification through REST POST to the
  * recipient of the Subscription Object.
- *
  */
 
 @Component
 public class SpringRestTemplate {
 
-    static Logger log = (Logger) LoggerFactory.getLogger(SpringRestTemplate.class);
+    private static final Logger LOGGER = (Logger) LoggerFactory.getLogger(SpringRestTemplate.class);
 
     private RestOperations rest;
 
@@ -57,32 +50,30 @@ public class SpringRestTemplate {
      * @return integer
      */
     public int postDataMultiValue(String notificationMeta, MultiValueMap<String, String> mapNotificationMessage, String headerContentMediaType) {
-        ResponseEntity<JsonNode> response = null;
+        ResponseEntity<JsonNode> response;
         try {
             HttpHeaders headers = new HttpHeaders();
             headers.setContentType(MediaType.valueOf(headerContentMediaType));
-            if(headerContentMediaType.equals(MediaType.APPLICATION_FORM_URLENCODED.toString())){ //"application/x-www-form-urlencoded"
-                HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<MultiValueMap<String, String>>(mapNotificationMessage, headers);
-                response = rest.postForEntity(notificationMeta, request , JsonNode.class );
-            }
-            else{
-                HttpEntity<String> request = new HttpEntity<String>(String.valueOf(((List<String>) mapNotificationMessage.get("")).get(0)), headers);
+            if (headerContentMediaType.equals(MediaType.APPLICATION_FORM_URLENCODED.toString())) { //"application/x-www-form-urlencoded"
+                HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(mapNotificationMessage, headers);
+                response = rest.postForEntity(notificationMeta, request, JsonNode.class);
+            } else {
+                HttpEntity<String> request = new HttpEntity<>(String.valueOf((mapNotificationMessage.get("")).get(0)), headers);
                 response = rest.postForEntity(notificationMeta, request, JsonNode.class);
             }
         } catch (Exception e) {
-            log.error(e.getMessage(), e);
+            LOGGER.error(e.getMessage(), e);
             try {
-            	return Integer.parseInt(e.getMessage());
-            }
-            catch (NumberFormatException error) {
-            	return HttpStatus.NOT_FOUND.value();
+                return Integer.parseInt(e.getMessage());
+            } catch (NumberFormatException error) {
+                return HttpStatus.NOT_FOUND.value();
             }
         }
         HttpStatus status = response.getStatusCode();
-        log.info("The response code after POST is : " + status);
+        LOGGER.debug("The response code after POST is : " + status);
         if (status == HttpStatus.OK) {
             JsonNode restCall = response.getBody();
-            log.info("The response Body is : " + restCall);
+            LOGGER.debug("The response Body is : " + restCall);
         }
         return response.getStatusCode().value();
     }
--- src/test/java/com/ericsson/ei/subscriptionhandler/test/SubscriptionHandlerTest.java
+++ src/test/java/com/ericsson/ei/subscriptionhandler/test/SubscriptionHandlerTest.java
@@ -16,115 +16,147 @@
 */
 package com.ericsson.ei.subscriptionhandler.test;
 
-import static org.junit.Assert.assertEquals;
-import java.io.File;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Set;
-
-import javax.annotation.PostConstruct;
-import org.apache.commons.io.FileUtils;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import com.ericsson.ei.App;
+import com.ericsson.ei.controller.model.QueryResponse;
 import com.ericsson.ei.exception.SubscriptionValidationException;
+import com.ericsson.ei.jmespath.JmesPathInterface;
 import com.ericsson.ei.mongodbhandler.MongoDBHandler;
+import com.ericsson.ei.queryservice.ProcessMissedNotification;
+import com.ericsson.ei.subscriptionhandler.InformSubscription;
 import com.ericsson.ei.subscriptionhandler.RunSubscription;
 import com.ericsson.ei.subscriptionhandler.SendMail;
-import com.ericsson.ei.subscriptionhandler.SubscriptionHandler;
+import com.ericsson.ei.subscriptionhandler.SpringRestTemplate;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.mongodb.MongoClient;
 import de.flapdoodle.embed.mongo.distribution.Version;
 import de.flapdoodle.embed.mongo.tests.MongodForTestsFactory;
+import org.apache.commons.io.FileUtils;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+
+import javax.annotation.PostConstruct;
+import java.io.File;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringBootTest(classes = App.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 public class SubscriptionHandlerTest {
+
+    private static final Logger LOGGER = (Logger) LoggerFactory.getLogger(SubscriptionHandlerTest.class);
+    private static final String aggregatedPath = "src/test/resources/AggregatedObject.json";
+    private static final String subscriptionPath = "src/test/resources/SubscriptionObject.json";
+    private static final String DB_NAME = "MissedNotification";
+    private static final String COLLECTION_NAME = "Notification";
+    private static final String REGEX = "^\"|\"$";
+    private static final String MISSED_NOTIFICATION_URL = "/queryMissedNotifications";
+    private static final int STATUS_OK = 200;
+    private static String aggregatedObject;
+    private static String subscriptionData;
+    private static String url;
+    private static String headerContentMediaType;
+    private static MongodForTestsFactory testsFactory;
+    private static MongoClient mongoClient = null;
 
     @Autowired
     private RunSubscription runSubscription;
 
     @Autowired
-    private SubscriptionHandler handler;
+    private MongoDBHandler mongoDBHandler;
+
+    @Autowired
+    private JmesPathInterface jmespath;
+
+    @Autowired
+    private InformSubscription subscription;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ProcessMissedNotification processMissedNotification;
+
+    @MockBean
+    private SpringRestTemplate springRestTemplate;
 
     @Autowired
     private SendMail sendMail;
 
-    private static String aggregatedPath = "src/test/resources/AggregatedObject.json";
-    private static String subscriptionPath = "src/test/resources/SubscriptionObject.json";
-    private static String subscriptionPathForEmail = "src/test/resources/SubscriptionObjectForEmailTest.json";
-    private static String aggregatedObject;
-    private static String subscriptionData;
-    private static String subscriptionDataEmail;
+    @Mock
+    private QueryResponse queryResponse;
 
-    static Logger log = (Logger) LoggerFactory.getLogger(SubscriptionHandlerTest.class);
-
-    @Autowired
-    private MongoDBHandler mongoDBHandler;
-
-    private static MongodForTestsFactory testsFactory;
-    static MongoClient mongoClient = null;
-
-    static String host = "localhost";
-    static int port = 27017;
-    private static String dataBaseName = "MissedNotification";
-    private static String collectionName = "Notification";
-
-    public static void setUpEmbeddedMongo() throws Exception {
+    public static void setUpEmbeddedMongo() throws JSONException, IOException {
         testsFactory = MongodForTestsFactory.with(Version.V3_4_1);
         mongoClient = testsFactory.newMongo();
-
-        try {
-            aggregatedObject = FileUtils.readFileToString(new File(aggregatedPath), "UTF-8");
-            subscriptionData = FileUtils.readFileToString(new File(subscriptionPath), "UTF-8");
-            subscriptionDataEmail = FileUtils.readFileToString(new File(subscriptionPathForEmail), "UTF-8");
-        } catch (Exception e) {
-            log.info(e.getMessage(), e);
-        }
+        aggregatedObject = FileUtils.readFileToString(new File(aggregatedPath), "UTF-8");
+        subscriptionData = FileUtils.readFileToString(new File(subscriptionPath), "UTF-8");
+        url = new JSONObject(subscriptionData).getString("notificationMeta").replaceAll(REGEX, "");
+        headerContentMediaType = new JSONObject(subscriptionData).getString("restPostBodyMediaType");
     }
 
     @BeforeClass
     public static void init() throws Exception {
         setUpEmbeddedMongo();
+        System.setProperty("notification.ttl.value", "1");
+    }
+
+    @AfterClass
+    public static void close() {
+        testsFactory.shutdown();
     }
 
     @PostConstruct
     public void initMocks() {
         mongoDBHandler.setMongoClient(mongoClient);
-        System.out.println("Database connected");
+        LOGGER.debug("Database connected");
     }
 
     @Test
     public void runSubscriptionOnObjectTest() {
         ObjectMapper mapper = new ObjectMapper();
-        JsonNode subscriptionJson = null;
-        ArrayNode requirementNode = null;
+        JsonNode subscriptionJson;
+        ArrayNode requirementNode;
         Iterator<JsonNode> requirementIterator = null;
         try {
             subscriptionJson = mapper.readTree(subscriptionData);
             requirementNode = (ArrayNode) subscriptionJson.get("requirements");
             requirementIterator = requirementNode.elements();
-        } catch (Exception e) {
-            log.error(e.getMessage(), e);
+        } catch (IOException e) {
+            LOGGER.error(e.getMessage(), e);
         }
-        boolean output = runSubscription.runSubscriptionOnObject(aggregatedObject, requirementIterator,
-                subscriptionJson);
+        boolean output = runSubscription.runSubscriptionOnObject(aggregatedObject, requirementIterator);
         assertEquals(output, true);
     }
 
     @Test
-    public void MissedNotificationTest() {
-        handler.extractConditions(aggregatedObject, subscriptionData);
-        Iterable<String> outputDoc = mongoDBHandler.getAllDocuments(dataBaseName, collectionName);
+    public void missedNotificationTest() throws IOException {
+        subscription.informSubscriber(aggregatedObject, new ObjectMapper().readTree(subscriptionData));
+        Iterable<String> outputDoc = mongoDBHandler.getAllDocuments(DB_NAME, COLLECTION_NAME);
         Iterator itr = outputDoc.iterator();
         String data = itr.next().toString();
         JsonNode jsonResult = null;
@@ -133,29 +165,70 @@ public class SubscriptionHandlerTest {
         try {
             expectedOutput = mapper.readTree(aggregatedObject);
             jsonResult = mapper.readTree(data);
-        } catch (Exception e) {
-            log.error(e.getMessage(), e);
+        } catch (IOException e) {
+            assertTrue(false);
+            LOGGER.error(e.getMessage(), e);
         }
         JsonNode output = jsonResult.get("AggregatedObject");
-        assertEquals(expectedOutput.toString(), output.toString());
+        assertEquals(expectedOutput, output);
+    }
+
+    @Test
+    public void missedNotificationWithTTLTest() throws IOException, InterruptedException {
+        subscription.informSubscriber(aggregatedObject, new ObjectMapper().readTree(subscriptionData));
+        Thread.sleep(70000);
+        assertTrue(mongoDBHandler.getAllDocuments(DB_NAME, COLLECTION_NAME).isEmpty());
     }
 
     @Test
     public void sendMailTest() {
-        Set<String> extRec = new HashSet<>();        
+        Set<String> extRec = new HashSet<>();
         String recievers = "asdf.hklm@ericsson.se, affda.fddfd@ericsson.com, sasasa.dfdfdf@fdad.com, abcd.defg@gmail.com";
         try {
             extRec = (sendMail.extractEmails(recievers));
         } catch (SubscriptionValidationException e) {
             // TODO Auto-generated catch block
-            log.error(e.getMessage(), e);
-        }      
+            LOGGER.error(e.getMessage(), e);
+        }
         assertEquals(String.valueOf(extRec.toArray().length), "4");
     }
-    
-    @AfterClass
-    public static void close() {
-        testsFactory.shutdown();
-        mongoClient.close();
+
+    @Test
+    public void testRestPostTrigger() throws IOException {
+        when(springRestTemplate.postDataMultiValue(url, mapNotificationMessage(), headerContentMediaType)).thenReturn(STATUS_OK);
+        subscription.informSubscriber(aggregatedObject, new ObjectMapper().readTree(subscriptionData));
+        verify(springRestTemplate, times(1)).postDataMultiValue(url, mapNotificationMessage(), headerContentMediaType);
+    }
+
+    @Test
+    public void testRestPostTriggerFailure() throws IOException {
+        subscription.informSubscriber(aggregatedObject, new ObjectMapper().readTree(subscriptionData));
+        verify(springRestTemplate, times(4)).postDataMultiValue(url, mapNotificationMessage(), headerContentMediaType);
+        assertFalse(mongoDBHandler.getAllDocuments(DB_NAME, COLLECTION_NAME).isEmpty());
+    }
+
+    @Test
+    public void testQueryMissedNotificationEndPoint() throws Exception {
+        String subscriptionName = new JSONObject(subscriptionData).getString("subscriptionName").replaceAll(REGEX, "");
+        JSONObject input = new JSONObject(aggregatedObject);
+        subscription.informSubscriber(aggregatedObject, new ObjectMapper().readTree(subscriptionData));
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get(MISSED_NOTIFICATION_URL)
+                .param("SubscriptionName", subscriptionName)).andReturn();
+        String response = result.getResponse().getContentAsString().replace("\\", "");
+        assertEquals("{\"responseEntity\":\"[" + input.toString().replace("\\", "") + "]\"}", response);
+        assertEquals(HttpStatus.OK.value(), result.getResponse().getStatus());
+    }
+
+    private MultiValueMap<String, String> mapNotificationMessage() throws IOException {
+        MultiValueMap<String, String> mapNotificationMessage = new LinkedMultiValueMap<>();
+        ArrayNode arrNode = (ArrayNode) new ObjectMapper().readTree(subscriptionData).get("notificationMessageKeyValues");
+        if (arrNode.isArray()) {
+            for (final JsonNode objNode : arrNode) {
+                mapNotificationMessage.add(objNode.get("formkey").toString().replaceAll(REGEX, ""),
+                        jmespath.runRuleOnEvent(objNode.get("formvalue").toString().replaceAll(REGEX, ""),
+                                aggregatedObject).toString().replaceAll(REGEX, ""));
+            }
+        }
+        return mapNotificationMessage;
     }
 }

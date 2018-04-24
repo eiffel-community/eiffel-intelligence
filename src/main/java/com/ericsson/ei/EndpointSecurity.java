a867ccf8b31e829eb4bd68609439e08513a553de
Christoffer Cortes Sjöwall <christoffer.cortes.sjowall@ericsson.com>
2018-04-24 14:33:27 +0200

    Fix

--- src/main/java/com/ericsson/ei/EndpointSecurity.java
+++ src/main/java/com/ericsson/ei/EndpointSecurity.java
@@ -59,7 +59,7 @@ public class EndpointSecurity extends WebSecurityConfigurerAdapter {
             LOGGER.info("LDAP security configuration is enabled");
             http
             .authorizeRequests()
-                .antMatchers("/login/auth/").permitAll()
+                .antMatchers("/auth/login").permitAll()
                 .anyRequest().authenticated()
             .and()
                 .sessionManagement()

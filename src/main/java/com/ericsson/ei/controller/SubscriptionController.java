2f7b3df7d3e57535b4a6c3726784fb1657f8e835
Mohammad Saifullah <mohammad.saifullah@ericsson.com>
2018-05-18 12:46:55 +0200

    error removal

--- src/main/java/com/ericsson/ei/controller/SubscriptionController.java
+++ src/main/java/com/ericsson/ei/controller/SubscriptionController.java
@@ -26,7 +26,7 @@ public interface SubscriptionController {
      * 
      */
     @RequestMapping(value = "", method = RequestMethod.GET)
-    public ResponseEntity<List<com.ericsson.ei.controller.model.Subscription>> getSubscriptions();
+    public ResponseEntity<List<String>> getSubscriptions();
 
     /**
      * Takes the subscription rules, the name for subscription and the user name of the person registering this subscription and saves the subscription in subscription database. The name needs to be unique.
@@ -36,34 +36,34 @@ public interface SubscriptionController {
     public ResponseEntity<?> createSubscription(
         @Valid
         @RequestBody
-        List<com.ericsson.ei.controller.model.Subscription> subscription);
+        List<String> string);
 
     /**
      * Modify an existing Subscription.
      * 
      */
     @RequestMapping(value = "", method = RequestMethod.PUT)
-    public ResponseEntity<com.ericsson.ei.controller.model.SubscriptionResponse> updateSubscriptions(
+    public ResponseEntity<SubscriptionResponse> updateSubscriptions(
         @Valid
         @RequestBody
-        List<com.ericsson.ei.controller.model.Subscription> subscription);
+        List<String> string);
 
     /**
      * Returns the subscription rules for given subscription name.
      * 
      */
     @RequestMapping(value = "/{subscriptionName}", method = RequestMethod.GET)
-    public ResponseEntity<List<com.ericsson.ei.controller.model.Subscription>> getSubscriptionById(
+    public ResponseEntity<List<String>> getSubscriptionById(
         @PathVariable(required = false)
-        String subscriptionName);
+        java.lang.String subscriptionName);
 
     /**
      * Removes the subscription from the database.
      * 
      */
     @RequestMapping(value = "/{subscriptionName}", method = RequestMethod.DELETE)
-    public ResponseEntity<com.ericsson.ei.controller.model.SubscriptionResponse> deleteSubscriptionById(
+    public ResponseEntity<SubscriptionResponse> deleteSubscriptionById(
         @PathVariable(required = false)
-        String subscriptionName);
+        java.lang.String subscriptionName);
 
 }

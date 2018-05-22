531070cd4c855e05662190bd6d6c5ed4db213001
Ned29 <nedvyha@gmail.com>
2018-05-22 16:20:56 +0300

    Fix information endpoint (#104)
    * FIX: information endpoint

--- src/main/java/com/ericsson/ei/mongodbhandler/MongoDBHandler.java
+++ src/main/java/com/ericsson/ei/mongodbhandler/MongoDBHandler.java
@@ -22,6 +22,8 @@ import java.util.concurrent.TimeUnit;
 
 import javax.annotation.PostConstruct;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Setter;
 import org.bson.Document;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -51,11 +53,9 @@ public class MongoDBHandler {
     static Logger log = (Logger) LoggerFactory.getLogger(MongoDBHandler.class);
     
     @Getter
-    MongoClient mongoClient;
-
-    public void setMongoClient(MongoClient mongoClient) {
-        this.mongoClient = mongoClient;
-    }
+    @Setter
+    @JsonIgnore
+    private MongoClient mongoClient;
 
     @Getter
     @Value("${spring.data.mongodb.host}")

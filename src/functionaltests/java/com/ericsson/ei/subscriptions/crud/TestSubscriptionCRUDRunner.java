c577678002d094dcf57dc85e0298b641f2050a60
Vasile Baluta <vasile.baluta@ericsson.com>
2018-05-31 13:10:55 +0200

    removed monochrome

--- src/functionaltests/java/com/ericsson/ei/subscriptions/crud/TestSubscriptionCRUDRunner.java
+++ src/functionaltests/java/com/ericsson/ei/subscriptions/crud/TestSubscriptionCRUDRunner.java
@@ -8,7 +8,7 @@ import cucumber.api.junit.Cucumber;
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src/functionaltests/resources/features/subscriptionCRUD.feature", glue = {
         "com.ericsson.ei.subscriptions.crud" }, plugin = { "pretty",
-                "html:target/cucumber-reports/TestSubscriptionCRUDRunner" }, monochrome = true)
+                "html:target/cucumber-reports/TestSubscriptionCRUDRunner" }, monochrome = false)
 public class TestSubscriptionCRUDRunner {
 
 }

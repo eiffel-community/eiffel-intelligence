34b7dc816f249163b9876937f173058976a98ad8
Vasile Baluta <vasile.baluta@ericsson.com>
2018-06-08 08:32:04 +0200

    update mongodb handler

--- src/main/java/com/ericsson/ei/mongodbhandler/MongoDBHandler.java
+++ src/main/java/com/ericsson/ei/mongodbhandler/MongoDBHandler.java
@@ -1,13 +1,10 @@
 /*
    Copyright 2017 Ericsson AB.
    For a full list of individual contributors, please see the commit history.
-
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
    You may obtain a copy of the License at
-
        http://www.apache.org/licenses/LICENSE-2.0
-
    Unless required by applicable law or agreed to in writing, software
    distributed under the License is distributed on an "AS IS" BASIS,
    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,10 +13,13 @@
 */
 package com.ericsson.ei.mongodbhandler;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.mongodb.BasicDBObject;
 import com.mongodb.Block;
 import com.mongodb.MongoClient;
+import com.mongodb.MongoCredential;
 import com.mongodb.MongoWriteException;
+import com.mongodb.ServerAddress;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.model.IndexOptions;
@@ -29,48 +29,53 @@ import com.mongodb.client.result.UpdateResult;
 import com.mongodb.util.JSON;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import javax.annotation.PostConstruct;
 
+import org.apache.commons.lang3.StringUtils;
 import org.bson.Document;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.mongo.MongoProperties;
 import org.springframework.stereotype.Component;
 
 import lombok.Getter;
+import lombok.Setter;
 
 @Component
 public class MongoDBHandler {
     static Logger log = (Logger) LoggerFactory.getLogger(MongoDBHandler.class);
 
-    @Getter
-    MongoClient mongoClient;
-
-    public void setMongoClient(MongoClient mongoClient) {
-        this.mongoClient = mongoClient;
-    }
+    @Autowired
+    private MongoProperties mongoProperties;
 
     @Getter
-    @Value("${spring.data.mongodb.host}")
-    private String host;
-
-    @Getter
-    @Value("${spring.data.mongodb.port}")
-    private int port;
+    @Setter
+    @JsonIgnore
+    private MongoClient mongoClient;
 
     // TODO establish connection automatically when Spring instantiate this
     // based on connection data in properties file
     @PostConstruct
     public void init() {
-        createConnection(host, port);
+        createConnection();
     }
 
     // Establishing the connection to mongodb and creating a collection
-    public void createConnection(String host, int port) {
-        mongoClient = new MongoClient(host, port);
+    private void createConnection() {
+        if (!StringUtils.isBlank(mongoProperties.getUsername())
+                && !StringUtils.isBlank(new String(mongoProperties.getPassword()))) {
+            ServerAddress address = new ServerAddress(mongoProperties.getHost(), mongoProperties.getPort());
+            MongoCredential credential = MongoCredential.createCredential(mongoProperties.getUsername(),
+                    mongoProperties.getDatabase(), mongoProperties.getPassword());
+            mongoClient = new MongoClient(address, Collections.singletonList(credential));
+        } else {
+            mongoClient = new MongoClient(mongoProperties.getHost(), mongoProperties.getPort());
+        }
     }
 
     /**

a568e8881dbf56ebe69c4eca6d49e8305f90170a
Christoffer Cortes Sjöwall <christoffer.cortes.sjowall@ericsson.com>
2018-06-14 16:05:26 +0200

    Added Mock SMTP and REST API

--- src/functionaltests/java/com/ericsson/ei/subscriptions/trigger/SubscriptionTriggerSteps.java
+++ src/functionaltests/java/com/ericsson/ei/subscriptions/trigger/SubscriptionTriggerSteps.java
@@ -1,20 +1,43 @@
 package com.ericsson.ei.subscriptions.trigger;
 
+import com.dumbster.smtp.SimpleSmtpServer;
+import com.dumbster.smtp.SmtpMessage;
 import com.ericsson.ei.utils.FunctionalTestBase;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import static org.mockserver.model.HttpRequest.request;
+import static org.mockserver.model.HttpResponse.response;
+import static org.mockserver.integration.ClientAndServer.startClientAndServer;
+
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
 import org.apache.commons.io.FileUtils;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 import org.json.JSONArray;
 import org.junit.Ignore;
+import org.mockserver.client.server.MockServerClient;
+import org.mockserver.integration.ClientAndServer;
+import org.mockserver.verify.VerificationTimes;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,13 +48,13 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.util.SocketUtils;
 
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import gherkin.deps.com.google.gson.JsonArray;
 import gherkin.deps.com.google.gson.JsonElement;
-import gherkin.deps.com.google.gson.JsonObject;
 import gherkin.deps.com.google.gson.JsonParser;
 
 @Ignore
@@ -42,6 +65,7 @@ public class SubscriptionTriggerSteps extends FunctionalTestBase {
     
 	@Autowired
 	private MockMvc mockMvc;
+	
 	MvcResult result;
 	ObjectMapper mapper = new ObjectMapper();
 	static JSONArray jsonArray = null;
@@ -114,6 +138,90 @@ public class SubscriptionTriggerSteps extends FunctionalTestBase {
     @Then("^Subscriptions were triggered$")
     public void check_subscriptions_were_triggered() throws Throwable {
     	LOGGER.debug("I have seven chickens that eat tigers.");
+    	
+    	//Mock SMTP
+    	try (SimpleSmtpServer dumbster = SimpleSmtpServer.start(SimpleSmtpServer.AUTO_SMTP_PORT)) {
+            
+    	    int port = dumbster.getPort();
+    	    String sender = "christoffer@ericsson.se";
+    	    String receiver = "anders@ericsson.se";
+    	    String subject = "Hello!";
+    	    String content = "This is a test for the mocked smtp server.";
+            sendMessage(port, sender, subject, content, receiver);
+            
+            List<SmtpMessage> emails = dumbster.getReceivedEmails();
+            assertEquals(emails.size(), 1);
+            
+            SmtpMessage email = emails.get(0);
+            LOGGER.debug("Email: "+email.toString());
+            assertEquals(email.getHeaderValue("From"), sender);
+        }
+    	
+    	//Mock REST API
+    	String baseURL ="127.0.0.1";
+    	int port = SocketUtils.findAvailableTcpPort();
+    	String path = "/stuff";
+    	
+    	
+    	ClientAndServer mockServer = startClientAndServer(port);
+    	
+    	MockServerClient mockClient = new MockServerClient(baseURL, port);
+    	mockClient
+    	.when(
+            request()
+                .withMethod("GET")
+                .withPath(path)
+        )
+        .respond(
+            response()
+                .withStatusCode(202)
+                .withBody(
+                    "Here is some stuff"
+                )
+        );
+    	
+    	HttpClient httpClient = HttpClients.createDefault();
+    	final HttpGet httpGet = new HttpGet("http://"+baseURL+":"+port+path);
+    	HttpResponse response = httpClient.execute(httpGet);
+    	
+    	String body = EntityUtils.toString(response.getEntity());
+    	LOGGER.debug("HttpGet: "+body);
+    	mockClient
+    	.verify(
+            request()
+                .withPath(path),
+            VerificationTimes.once()
+        );
+    	
+    	mockClient.close();
+    	mockServer.stop();
     }
+    
+    private void sendMessage(int port, String from, String subject, String body, String to) throws MessagingException {
+        Properties mailProps = getMailProperties(port);
+        Session session = Session.getInstance(mailProps, null);
+        //session.setDebug(true);
 
+        MimeMessage msg = createMessage(session, from, to, subject, body);
+        Transport.send(msg);
+    }
+    
+    private Properties getMailProperties(int port) {
+        Properties mailProps = new Properties();
+        mailProps.setProperty("mail.smtp.host", "localhost");
+        mailProps.setProperty("mail.smtp.port", "" + port);
+        mailProps.setProperty("mail.smtp.sendpartial", "true");
+        return mailProps;
+    }
+    
+    private MimeMessage createMessage(
+        Session session, String from, String to, String subject, String body) throws MessagingException {
+        MimeMessage msg = new MimeMessage(session);
+        msg.setFrom(new InternetAddress(from));
+        msg.setSubject(subject);
+        msg.setSentDate(new Date());
+        msg.setText(body);
+        msg.setRecipient(Message.RecipientType.TO, new InternetAddress(to));
+        return msg;
+    }
 }

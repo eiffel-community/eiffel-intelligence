ae5e098f30b8fe7e3dcc2b7d5585b14dc6b157c4
eztyhva <valentin.tyhonov@ericsson.com>
2018-06-15 13:11:13 +0300

    Added functional tests for Rules Checker

--- src/functionaltests/java/com/ericsson/ei/subscriptions/ruleCheck/RuleCheckSteps.java
+++ src/functionaltests/java/com/ericsson/ei/subscriptions/ruleCheck/RuleCheckSteps.java
@@ -1,5 +1,6 @@
 package com.ericsson.ei.subscriptions.ruleCheck;
 
+import com.ericsson.ei.controller.RuleCheckController;
 import com.ericsson.ei.utils.FunctionalTestBase;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
@@ -9,14 +10,15 @@ import org.junit.Ignore;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.http.MediaType;
+import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
 import java.io.File;
 
 import static org.junit.Assert.assertEquals;
+import static org.skyscreamer.jsonassert.JSONAssert.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
 @Ignore
 @AutoConfigureMockMvc
@@ -25,9 +27,22 @@ public class RuleCheckSteps extends FunctionalTestBase {
     private static final String TEST_RESOURCES_PATH = "src/functionaltests/resources/ruleCheck/";
 
     @Autowired
+    private RuleCheckController ruleCheckController;
+
+    @Autowired
     private MockMvc mockMvc;
 
     private MvcResult mvcResult;
+
+    @Given("^rules checking is enabled$")
+    public void rules_checking_is_enabled() throws Throwable {
+        ReflectionTestUtils.setField(ruleCheckController, "testEnable", true);
+    }
+
+    @Given("^rules checking is not enabled$")
+    public void rules_checking_is_not_enabled() throws Throwable {
+        ReflectionTestUtils.setField(ruleCheckController, "testEnable", false);
+    }
 
     @When("^make a POST request with JMESPath rule \"([^\"]*)\" and JSON object \"([^\"]*)\" to the REST API \"([^\"]*)\"$")
     public void make_a_POST_request_with_JMESPath_rule_and_JSON_object_to_the_REST_API(String ruleFilename, String eventFilename, String endpoint) throws Throwable {
@@ -40,12 +55,6 @@ public class RuleCheckSteps extends FunctionalTestBase {
             .contentType(MediaType.APPLICATION_JSON))
             .andReturn();
     }
-    ///Scenario:1 ends ===============================================================================
-
-    @Given("^rules checking is enabled$")
-    public void rules_checking_is_enabled() throws Throwable {
-
-    }
 
     @When("^make a POST request with list of JMESPath rules and list of JSON objects \"([^\"]*)\" to the REST API \"([^\"]*)\"$")
     public void make_a_POST_request_with_list_of_JMESPath_rules_and_list_of_JSON_objects_to_the_REST_API(String requestBodyFilename, String endpoint) throws Throwable {
@@ -54,16 +63,14 @@ public class RuleCheckSteps extends FunctionalTestBase {
             .accept(MediaType.APPLICATION_JSON)
             .content(requestBody)
             .contentType(MediaType.APPLICATION_JSON))
-            .andDo(print())
             .andReturn();
     }
-    ///Scenario:2 ends ===============================================================================
 
     @Then("^get response code of (\\d+) and content \"([^\"]*)\"$")
     public void get_response_code_of_and_content(int statusCode, String contentFilename) throws Throwable {
         String responseBody = FileUtils.readFileToString(new File(TEST_RESOURCES_PATH + contentFilename), "UTF-8");
         assertEquals(statusCode, mvcResult.getResponse().getStatus());
-        assertEquals(responseBody, mvcResult.getResponse().getContentAsString());
+        assertEquals(responseBody, mvcResult.getResponse().getContentAsString(), true);
     }
 
 }
--- src/functionaltests/resources/features/ruleCheck.feature
+++ src/functionaltests/resources/features/ruleCheck.feature
@@ -17,31 +17,27 @@
 ## (Comments)
 #Sample Feature Definition Template
 @tag
-Feature: Test Check Rule
+Feature: Test Rules Checker
 
   @tag1
   Scenario: Execute JMESPath rule on JSON object
     When make a POST request with JMESPath rule "extractionRule.txt" and JSON object "artCEvent.json" to the REST API "/rules/rule-check"
-    Then get response code of 200 and content "resultOfExtraction.txt"
+    Then get response code of 200 and content "resultOfExtraction.json"
 
-  #@tag2
+  @tag2
   Scenario: Execute list of JMESPath rules on list of JSON objects
     Given rules checking is enabled
     When make a POST request with list of JMESPath rules and list of JSON objects "inputData.json" to the REST API "/rules/rule-check/aggregation"
-    Then get response code of 200 and content "resultList.json"
+    Then get response code of 200 and content "resultAggregatedObject.json"
 
-#  @tag3
-#  Scenario: Update subscription using REST API by PUT method and validate updation
-#    Given The REST API "/subscriptions" is up and running
-#    When I make a PUT request with modified notificationType as "MAIL" to REST API "/subscriptions"
-#    Then I get response code of 200 for successful updation
-#    And  I can validate modified notificationType "MAIL" with GET request at "/subscriptions/Subscription_Test"
-#
-#  #@tag4
-#  Scenario: Delete subscription using REST API by DELETE method and validate deletion
-#    Given The REST API "/subscriptions" is up and running
-#    When  I make a DELETE request with subscription name "Subscription_Test" to the  subscription REST API "/subscriptions/"
-#    Then  I get response code of 200 for successful delete
-#    And   My GET request with subscription name "Subscription_Test" at REST API "/subscriptions/" returns empty String "[]"
+  @tag3
+  Scenario: Execute incorrect list of JMESPath rules on list of JSON objects
+    Given rules checking is enabled
+    When make a POST request with list of JMESPath rules and list of JSON objects "incorrectInputData.json" to the REST API "/rules/rule-check/aggregation"
+    Then get response code of 400 and content "badRequestResponse.json"
 
-
+  @tag4
+  Scenario: Execute list of JMESPath rules on list of JSON objects, when rules checking is not enabled
+    Given rules checking is not enabled
+    When make a POST request with list of JMESPath rules and list of JSON objects "inputData.json" to the REST API "/rules/rule-check/aggregation"
+    Then get response code of 503 and content "environmentDisabledResponse.json"

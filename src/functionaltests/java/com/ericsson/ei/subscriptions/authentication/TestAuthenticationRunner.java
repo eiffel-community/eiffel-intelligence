b33aa67be79bd01d190956c2dafd70682d714a3f
valentin <valentin.tyhonov@gmail.com>
2018-06-15 14:52:30 +0300

    Added functional test for authentication (#117)

---
+++ src/functionaltests/java/com/ericsson/ei/subscriptions/authentication/TestAuthenticationRunner.java
@@ -0,0 +1,15 @@
+package com.ericsson.ei.subscriptions.authentication;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src/functionaltests/resources/features/authentication.feature", glue = {
+    "com.ericsson.ei.subscriptions.authentication" }, plugin = { "pretty",
+    "html:target/cucumber-reports/TestSubscriptionCRUDRunner" }, monochrome = false)
+public class TestAuthenticationRunner {
+
+
+
+}

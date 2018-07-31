1fc158de81c33ee73cb3ac735a1bacc41185a22d
SAIF <33659596+sericcson@users.noreply.github.com>
2018-07-31 15:18:51 +0200

    Threading and waitlist (#135)
    * JMESPath REST API
    
    * JMESPath  API with fixed reviews
    
    * partial work jmespath rest
    
    * JMESPath API with RAML 0.8 specification
    
    * remove componet comment
    
    * replace tab with spaces
    
    * fix usage examples
    
    * prepare for new asserts in flow test
    
    * fix unit tests
    
    * Subscription for individual events
    
    * pom file
    
    * subscription individual objects
    
    * pom
    
    * error removal
    
    * Subscription Individual Object
    
    * deleting unused import
    
    * jmespath comment correction
    
    * space removal and new clean build
    
    * review changes
    
    * review change file deleted
    
    * review change
    
    * review changes
    
    * log.info to log.error
    
    * required changes
    
    * raml
    
    * raml files
    
    * resolving conflict
    
    * self generated code raml
    
    * controllers auto generated
    
    * auto generated
    
    * removing ttl
    
    * adding ttl
    
    * copied pom
    
    * changes in app.properties
    
    * resolving conflict with upstream
    
    * adding more files
    
    * added time lapse variable
    
    * rebased...
    
    * new clone and update
    
    * reviewed changes
    
    * review changes-evasiba
    
    * review changes 2:evasiba
    
    * handling of authorization by Subscription REST API: 9584
    
    * changing property name
    
    * pom updated
    
    * method change
    
    * reviewed changes-1
    
    * some review changes regarding comments
    
    * some changes ´to get pass throguh tests
    
    * review changes
    
    * Update application.properties
    
    * Fix test
    
    * development
    
    * changes
    
    * code changes related to story 9561
    
    * error
    
    * error removal
    
    * requested changes
    
    * reviewed change
    
    * reviewed changes
    
    * added random port in FunctionalTestBase
    
    * Add threading and waitlist tests
    
    - Still in progress
    
    * working code
    
    * changing test resources from location to properties
    
    * some changes in time units
    
    * Update ThreadingAndWaitlistRepeatSteps.java
    
    * Update ThreadingAndWaitlistRepeatSteps.java
    
    * Update ThreadingAndWaitlistRepeatSteps.java
    
    * timing related changes
    
    * reviewed changes
    
    * reviewed change

---
+++ src/functionaltests/java/com/ericsson/ei/threadingAndWaitlistRepeat/ThreadingAndWaitlistRepeatRunner.java
@@ -0,0 +1,15 @@
+package com.ericsson.ei.threadingAndWaitlistRepeat;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.TestPropertySource;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src/functionaltests/resources/features/threadingAndWaitlistRepeat.feature", glue = {
+        "com.ericsson.ei.threadingAndWaitlistRepeat" }, plugin = {
+                "html:target/cucumber-reports/ThreadingAndWaitlistRepeatRunner" }, monochrome = false)
+public class ThreadingAndWaitlistRepeatRunner {
+
+}

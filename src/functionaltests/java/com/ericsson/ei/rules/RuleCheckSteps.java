94c0ef70bb91199bc5de91cc964c3ce5a9ffaff8
Erik <erik.edling@ericsson.com>
2018-08-13 15:36:22 +0200

    Replace mockmvc in rule-check

--- src/functionaltests/java/com/ericsson/ei/rules/RuleCheckSteps.java
+++ src/functionaltests/java/com/ericsson/ei/rules/RuleCheckSteps.java
@@ -2,28 +2,27 @@ package com.ericsson.ei.rules;
 
 import com.ericsson.ei.controller.RuleCheckController;
 import com.ericsson.ei.utils.FunctionalTestBase;
+import com.ericsson.ei.utils.HttpRequest;
+import com.ericsson.ei.utils.HttpRequest.HttpMethod;
+
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+
 import org.apache.commons.io.FileUtils;
 import org.json.JSONArray;
 import org.json.JSONObject;
+import org.json.JSONTokener;
 import org.junit.Ignore;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.http.MediaType;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.util.ReflectionTestUtils;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
-
 import java.io.File;
-
 import static org.junit.Assert.assertEquals;
 import static org.skyscreamer.jsonassert.JSONAssert.assertEquals;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 @Ignore
-@AutoConfigureMockMvc
 public class RuleCheckSteps extends FunctionalTestBase {
 
     private static final String TEST_RESOURCES_PATH = "src/test/resources";
@@ -31,13 +30,13 @@ public class RuleCheckSteps extends FunctionalTestBase {
     @Autowired
     private RuleCheckController ruleCheckController;
 
-    @Autowired
-    private MockMvc mockMvc;
-
-    private MvcResult mvcResult;
-
     private String rules;
     private String events;
+
+    @LocalServerPort
+    private int applicationPort;
+    private String hostName = getHostName();
+    private ResponseEntity response;
 
     @Given("^rules checking is enabled$")
     public void rules_checking_is_enabled() throws Throwable {
@@ -57,12 +56,15 @@ public class RuleCheckSteps extends FunctionalTestBase {
 
     @When("^make a POST request to the REST API \"([^\"]*)\" with request parameter \"([^\"]*)\"$")
     public void make_a_POST_request_to_the_REST_API_with_request_parameter(String endpoint, String requestParam) throws Throwable {
-        mvcResult = mockMvc.perform(post(endpoint)
-            .param(requestParam, rules)
-            .accept(MediaType.APPLICATION_JSON)
-            .content(events)
-            .contentType(MediaType.APPLICATION_JSON))
-            .andReturn();
+        HttpRequest postRequest = new HttpRequest(HttpMethod.POST);
+        response = postRequest.setPort(applicationPort)
+                .setUrl(hostName)
+                .setHeaders("content-type", "application/json")
+                .setHeaders("Accept", "application/json")
+                .setEndpoint(endpoint)
+                .setBody(events)
+                .setParam(requestParam, rules)
+                .performRequest();
     }
 
     @When("^make a POST request to the REST API \"([^\"]*)\"$")
@@ -71,22 +73,36 @@ public class RuleCheckSteps extends FunctionalTestBase {
             .put("listRulesJson", new JSONArray(rules))
             .put("listEventsJson", new JSONArray(events))
             .toString();
-        mvcResult = mockMvc.perform(post(endpoint)
-            .accept(MediaType.APPLICATION_JSON)
-            .content(requestBody)
-            .contentType(MediaType.APPLICATION_JSON))
-            .andReturn();
+
+        HttpRequest postRequest = new HttpRequest(HttpMethod.POST);
+        response = postRequest.setPort(applicationPort)
+                .setUrl(hostName)
+                .setHeaders("content-type", "application/json")
+                .setHeaders("Accept", "application/json")
+                .setEndpoint(endpoint)
+                .setBody(requestBody)
+                .performRequest();
     }
 
     @Then("^get response code of (\\d+)$")
     public void get_response_code_of(int statusCode) throws Throwable {
-        assertEquals(statusCode, mvcResult.getResponse().getStatus());
+        assertEquals(statusCode, response.getStatusCodeValue());
     }
 
     @Then("^get content \"([^\"]*)\"$")
     public void get_content(String contentFileName) throws Throwable {
         String responseBody = FileUtils.readFileToString(new File(TEST_RESOURCES_PATH + contentFileName), "UTF-8");
-        assertEquals(responseBody, mvcResult.getResponse().getContentAsString(), true);
+
+        Object expectedResponse = new JSONTokener(responseBody).nextValue();
+        if (expectedResponse instanceof JSONArray) {
+            JSONArray expectedArray = new JSONArray(responseBody);
+            JSONArray responseArray = new JSONArray(response.getBody().toString());
+            assertEquals(expectedArray, responseArray, true);
+        } else {
+            JSONObject expectedObject = new JSONObject(responseBody);
+            JSONObject responseObject = new JSONObject(response.getBody().toString());
+            assertEquals(expectedObject, responseObject, true);
+        }
     }
 
 }

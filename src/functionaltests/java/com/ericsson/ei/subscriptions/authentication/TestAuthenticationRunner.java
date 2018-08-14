be4250a88bb40cb6be98ecad6ea582910497543d
eznedan <andriy.nedvyha@ericsson.com>
2018-08-14 15:25:34 +0300

    review fixes

--- src/functionaltests/java/com/ericsson/ei/subscriptions/authentication/TestAuthenticationRunner.java
+++ src/functionaltests/java/com/ericsson/ei/subscriptions/authentication/TestAuthenticationRunner.java
@@ -5,7 +5,7 @@ import cucumber.api.CucumberOptions;
 
 @CucumberOptions(features = "src/functionaltests/resources/features/authentication.feature", glue = {
         "com.ericsson.ei.subscriptions.authentication"}, plugin = {"pretty",
-        "html:target/cucumber-reports/TestSubscriptionCRUDRunner"})
+        "html:target/cucumber-reports/TestAuthenticationRunner"})
 public class TestAuthenticationRunner extends BaseRunner {
 
 }
--- src/functionaltests/java/com/ericsson/ei/subscriptions/repeatHandler/TestSubscriptionRepeatHandlerRunner.java
+++ src/functionaltests/java/com/ericsson/ei/subscriptions/repeatHandler/TestSubscriptionRepeatHandlerRunner.java
@@ -5,7 +5,7 @@ import cucumber.api.CucumberOptions;
 
 @CucumberOptions(features = "src/functionaltests/resources/features/subscriptionRepeatHandler.feature", glue = {
         "com.ericsson.ei.subscriptions.repeatHandler"}, plugin = {"pretty",
-        "html:target/cucumber-reports/TestSubscriptionRepeatHandleRunner"})
+        "html:target/cucumber-reports/TestSubscriptionRepeatHandlerRunner"})
 public class TestSubscriptionRepeatHandlerRunner extends BaseRunner {
 
 }

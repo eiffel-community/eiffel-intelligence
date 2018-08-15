cdbf9e4df14ab48ab8c0d54467d8ce8a56b5d58e
e-pettersson-ericsson <38855976+e-pettersson-ericsson@users.noreply.github.com>
2018-08-15 08:31:20 +0200

    Replace mockMvc in subscription bulk tests (#155)

--- src/functionaltests/java/com/ericsson/ei/subscriptions/bulk/SubscriptionBulkSteps.java
+++ src/functionaltests/java/com/ericsson/ei/subscriptions/bulk/SubscriptionBulkSteps.java
@@ -2,6 +2,8 @@ package com.ericsson.ei.subscriptions.bulk;
 
 import com.ericsson.ei.controller.model.GetSubscriptionResponse;
 import com.ericsson.ei.utils.FunctionalTestBase;
+import com.ericsson.ei.utils.HttpRequest;
+import com.ericsson.ei.utils.HttpRequest.HttpMethod;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import cucumber.api.java.en.Given;
@@ -11,31 +13,27 @@ import org.apache.commons.io.FileUtils;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.junit.Ignore;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.TestPropertySource;
 
 import java.io.File;
 
 import static org.junit.Assert.assertEquals;
 
 @Ignore
-@AutoConfigureMockMvc
+@TestPropertySource(properties = {"logging.level.com.ericsson.ei.subscriptions.bulk=DEBUG"})
 public class SubscriptionBulkSteps extends FunctionalTestBase {
 
     private static final String TEST_RESOURCES_PATH = "src/functionaltests/resources";
 
-    @Autowired
-    private MockMvc mockMvc;
+    @LocalServerPort
+    private int port;
 
-    private MvcResult mvcResult;
-
+    private String hostName = getHostName();
     private JSONArray subscriptions;
-
     private JSONArray retrievedSubscriptions;
+    private ResponseEntity response;
 
     @Given("^file with subscriptions \"([^\"]*)\"$")
     public void file_with_subscriptions(String subscriptionsFileName) throws Throwable {
@@ -45,69 +43,102 @@ public class SubscriptionBulkSteps extends FunctionalTestBase {
 
     @When("^make a POST request with list of subscriptions to the subscription REST API \"([^\"]*)\"$")
     public void make_a_POST_request_with_list_of_subscriptions_to_the_subscription_REST_API(String endpoint) throws Throwable {
-        mvcResult = mockMvc.perform(MockMvcRequestBuilders.post(endpoint)
-            .accept(MediaType.APPLICATION_JSON)
-            .content(subscriptions.toString())
-            .contentType(MediaType.APPLICATION_JSON))
-            .andReturn();
+        HttpRequest postRequest = new HttpRequest(HttpMethod.POST);
+        response = postRequest.setHost(hostName)
+                .setPort(port)
+                .setEndpoint(endpoint)
+                .setHeaders("content-type", "application/json")
+                .setHeaders("Accept", "application/json")
+                .setBody(subscriptions.toString())
+                .performRequest();
     }
 
     @When("^make a GET request with list of subscriptions names \"([^\"]*)\" to the subscription REST API \"([^\"]*)\"$")
-    public void make_a_GET_request_with_list_of_subscriptions_names_to_the_subscription_REST_API(String subscriptionsNamesList, String endpoint) throws Throwable {
-        mvcResult = mockMvc.perform(MockMvcRequestBuilders.get(endpoint + "/" + subscriptionsNamesList)
-            .accept(MediaType.APPLICATION_JSON))
-            .andReturn();
+    public void make_a_GET_request_with_list_of_subscriptions_names_to_the_subscription_REST_API(
+            String subscriptionsNamesList, String endpoint) throws Throwable {
+
+        HttpRequest getRequest = new HttpRequest(HttpMethod.GET);
+        response = getRequest.setHost(hostName)
+                .setPort(port)
+                .setEndpoint(endpoint + "/" + subscriptionsNamesList)
+                .setHeaders("Accept", "application/json")
+                .performRequest();
     }
 
     @When("^make a DELETE request with list of subscriptions names \"([^\"]*)\" to the subscription REST API \"([^\"]*)\"$")
-    public void make_a_DELETE_request_with_list_of_subscriptions_names_to_the_subscription_REST_API(String subscriptionsNamesList, String endpoint) throws Throwable {
-        mvcResult = mockMvc.perform(MockMvcRequestBuilders.delete(endpoint + "/" + subscriptionsNamesList)
-            .accept(MediaType.APPLICATION_JSON))
-            .andReturn();
+    public void make_a_DELETE_request_with_list_of_subscriptions_names_to_the_subscription_REST_API(
+            String subscriptionsNamesList, String endpoint) throws Throwable {
+
+        HttpRequest deleteRequest = new HttpRequest(HttpMethod.DELETE);
+        response = deleteRequest.setHost(hostName)
+                .setPort(port)
+                .setEndpoint(endpoint + "/" + subscriptionsNamesList)
+                .setHeaders("Accept", "application/json")
+                .performRequest();
     }
 
     @When("^make a PUT request with list of subscriptions to the subscription REST API \"([^\"]*)\"$")
-    public void make_a_PUT_request_with_list_of_subscriptions_to_the_subscription_REST_API(String endpoint) throws Throwable {
-        mvcResult = mockMvc.perform(MockMvcRequestBuilders.put(endpoint)
-            .accept(MediaType.APPLICATION_JSON)
-            .content(subscriptions.toString())
-            .contentType(MediaType.APPLICATION_JSON))
-            .andReturn();
+    public void make_a_PUT_request_with_list_of_subscriptions_to_the_subscription_REST_API(
+            String endpoint) throws Throwable {
+
+        HttpRequest putRequest = new HttpRequest(HttpMethod.PUT);
+        response = putRequest.setHost(hostName)
+                .setPort(port)
+                .setEndpoint(endpoint)
+                .setHeaders("content-type", "application/json")
+                .setHeaders("Accept", "application/json")
+                .setBody(subscriptions.toString())
+                .performRequest();
     }
 
     @Then("^get response code of (\\d+)$")
     public void get_response_code_of(int statusCode) throws Throwable {
-        assertEquals(statusCode, mvcResult.getResponse().getStatus());
+        assertEquals(statusCode, response.getStatusCode().value());
     }
 
     @Then("^get in response content (\\d+) found subscriptions and not found subscription name \"([^\"]*)\"$")
-    public void get_in_response_content_found_subscriptions_and_not_found_subscription_name(int foundSubscriptionsNumber, String notFoundSubscriptionsName) throws Throwable {
-        GetSubscriptionResponse response = new ObjectMapper().readValue(mvcResult.getResponse().getContentAsString(), GetSubscriptionResponse.class);
-        assertEquals(foundSubscriptionsNumber, response.getFoundSubscriptions().size());
-        assertEquals(notFoundSubscriptionsName, response.getNotFoundSubscriptions().get(0));
+    public void get_in_response_content_found_subscriptions_and_not_found_subscription_name(
+            int foundSubscriptionsNumber, String notFoundSubscriptionsName) throws Throwable {
+
+        GetSubscriptionResponse subscriptionResponse = new ObjectMapper().readValue(
+                response.getBody().toString(), GetSubscriptionResponse.class);
+
+        assertEquals(foundSubscriptionsNumber,
+                subscriptionResponse.getFoundSubscriptions().size());
+        assertEquals(notFoundSubscriptionsName,
+                subscriptionResponse.getNotFoundSubscriptions().get(0));
     }
 
     @Then("^get in response content subscription \"([^\"]*)\"$")
     public void get_in_response_content_subscription_and_reason(String subscriptionName) throws Throwable {
-        JSONObject response = new JSONArray(mvcResult.getResponse().getContentAsString()).getJSONObject(0);
-        assertEquals(subscriptionName, response.getString("subscription"));
+        JSONObject jsonResponse = new JSONArray(response.getBody().toString()).getJSONObject(0);
+        assertEquals(subscriptionName, jsonResponse.getString("subscription"));
     }
 
     @Then("^number of retrieved subscriptions using REST API \"([^\"]*)\" is (\\d+)$")
-    public void number_of_retrieved_subscriptions_using_REST_API_is(String endpoint, int subscriptionsNumber) throws Throwable {
-        mvcResult = mockMvc.perform(MockMvcRequestBuilders.get(endpoint).accept(MediaType.APPLICATION_JSON)).andReturn();
-        retrievedSubscriptions = new JSONArray(mvcResult.getResponse().getContentAsString());
+    public void number_of_retrieved_subscriptions_using_REST_API_is(
+            String endpoint, int subscriptionsNumber) throws Throwable {
+
+        HttpRequest getRequest = new HttpRequest(HttpMethod.GET);
+        response = getRequest.setHost(hostName)
+                .setPort(port)
+                .setEndpoint(endpoint)
+                .setHeaders("Accept", "application/json")
+                .performRequest();
+        retrievedSubscriptions = new JSONArray(response.getBody().toString());
         assertEquals(subscriptionsNumber, retrievedSubscriptions.length());
     }
 
     @Then("^retrieved subscriptions are same as given$")
     public void retrieved_subscriptions_are_same_as_given() throws Throwable {
         for (int i = 0; i < subscriptions.length(); i++) {
-            assertEquals(subscriptions.getJSONObject(i).get("subscriptionName"), retrievedSubscriptions.getJSONObject(i).get("subscriptionName"));
-            assertEquals(subscriptions.getJSONObject(i).get("notificationType"), retrievedSubscriptions.getJSONObject(i).get("notificationType"));
-            assertEquals(subscriptions.getJSONObject(i).get("notificationMeta"), retrievedSubscriptions.getJSONObject(i).get("notificationMeta"));
+            assertEquals(subscriptions.getJSONObject(i).get("subscriptionName"),
+                    retrievedSubscriptions.getJSONObject(i).get("subscriptionName"));
+            assertEquals(subscriptions.getJSONObject(i).get("notificationType"),
+                    retrievedSubscriptions.getJSONObject(i).get("notificationType"));
+            assertEquals(subscriptions.getJSONObject(i).get("notificationMeta"),
+                    retrievedSubscriptions.getJSONObject(i).get("notificationMeta"));
             assertEquals(true, retrievedSubscriptions.getJSONObject(i).has("userName"));
         }
     }
-
 }

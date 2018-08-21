ac0a1dc40cfe20da10712a5d4ece794cbcf91189
eroohen <henning.roos@ericsson.com>
2018-08-21 16:14:55 +0200

    Fix review comments

--- src/test/java/com/ericsson/ei/queryservice/test/TestProcessQueryParams.java
+++ src/test/java/com/ericsson/ei/queryservice/test/TestProcessQueryParams.java
@@ -3,7 +3,6 @@ package com.ericsson.ei.queryservice.test;
 import com.ericsson.ei.App;
 import com.ericsson.ei.queryservice.ProcessAggregatedObject;
 import com.ericsson.ei.queryservice.ProcessQueryParams;
-import com.fasterxml.jackson.core.io.JsonEOFException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.qpid.util.FileUtils;
@@ -38,7 +37,6 @@ public class TestProcessQueryParams {
     private static final String REQUEST = "{\"criteria\":{\"testCaseExecutions.testCase.verdict\":\"PASSED\"}}";
     private static final String QUERY_WITH_CRITERIA_AND_OPTIONS = "{\"criteria\" :{\"testCaseExecutions.testCase.verdict\":\"PASSED\", \"testCaseExecutions.testCase.id\":\"TC5\" }, \"options\" :{ \"id\": \"6acc3c87-75e0-4b6d-88f5-b1a5d4e62b43\"} }";
     private static final String QUERY_WITH_CRITERIA = "{\"criteria\" :{\"testCaseExecutions.testCase.verdict\":\"PASSED\", \"testCaseExecutions.testCase.id\":\"TC5\" }}";
-    private static final String QUERY_WITH_WRONG_FORMAT = "{\"criteria\" :{\"testCaseExecutions.testCase.verdict\":\"PASSED\", \"testCaseExecutions.testCase.id\":\"TC5\" }, \"options\" :{ \"id\": \"6acc3c87-75e0-4b6d-88f5-b1a5d4e62b43\" }";
     private static final String DATA_BASE_NAME = "eiffel_intelligence";
     private static final String AGGREGATION_COLLECTION_NAME = "aggregated_objects";
     private static JSONArray expected;
@@ -66,7 +64,8 @@ public class TestProcessQueryParams {
             JSONObject options = (JSONObject) query.get("options");
 
             String request = "{ \"$and\" : [ " + criteria.toString() + "," + options.toString() + " ] }";
-            when(processAggregatedObject.processQueryAggregatedObject(request, DATA_BASE_NAME, AGGREGATION_COLLECTION_NAME)).thenReturn(expected);
+            when(processAggregatedObject.processQueryAggregatedObject(
+                    request, DATA_BASE_NAME, AGGREGATION_COLLECTION_NAME)).thenReturn(expected);
             JSONArray result = processQueryParams.filterFormParam(criteria, options);
             assertEquals(expected, result);
         } catch (Exception e) {
@@ -77,11 +76,12 @@ public class TestProcessQueryParams {
     @Test
     public void testFilterFormParamWithOnlyCriteria() throws IOException {
         try {
-            JSONObject query = new JSONObject(QUERY_WITH_CRITERIA);
-            JSONObject criteria = (JSONObject) query.get("criteria");
+            JSONObject queryBody = new JSONObject(QUERY_WITH_CRITERIA);
+            JSONObject criteria = (JSONObject) queryBody.get("criteria");
             JSONObject options = null;
 
-            when(processAggregatedObject.processQueryAggregatedObject(criteria.toString(), DATA_BASE_NAME, AGGREGATION_COLLECTION_NAME)).thenReturn(expected);
+            when(processAggregatedObject.processQueryAggregatedObject(
+                    criteria.toString(), DATA_BASE_NAME, AGGREGATION_COLLECTION_NAME)).thenReturn(expected);
             JSONArray result = processQueryParams.filterFormParam(criteria, options);
             assertEquals(expected, result);
         } catch (Exception e) {
@@ -92,7 +92,8 @@ public class TestProcessQueryParams {
     @Test
     public void testFilterQueryParam() throws IOException {
         JsonNode criteria = mapper.readValue(REQUEST, JsonNode.class).get("criteria");
-        when(processAggregatedObject.processQueryAggregatedObject(criteria.toString(), DATA_BASE_NAME, AGGREGATION_COLLECTION_NAME)).thenReturn(expected);
+        when(processAggregatedObject.processQueryAggregatedObject(
+                criteria.toString(), DATA_BASE_NAME, AGGREGATION_COLLECTION_NAME)).thenReturn(expected);
         JSONArray result = processQueryParams.filterQueryParam(REQUEST);
         assertEquals(expected, result);
     }

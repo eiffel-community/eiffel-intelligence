4be9ec198ed3698803f6cd69501296463129876f
Erik Edling <erik.edling@ericsson.com>
2018-10-02 09:40:19 +0200

    Fix comment

--- src/functionaltests/java/com/ericsson/ei/query/QueryAggregatedObjectsTestSteps.java
+++ src/functionaltests/java/com/ericsson/ei/query/QueryAggregatedObjectsTestSteps.java
@@ -23,7 +23,6 @@ import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-import cucumber.api.java.Before;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;

eaeced06a762c12dd75622b2c5df26316887e8d3
evasiba <vasile.baluta@ericsson.com>
2018-11-09 14:59:42 +0100

    change flow to run as system test with local ER

--- src/test/java/com/ericsson/ei/flowtests/FlowTest.java
+++ src/test/java/com/ericsson/ei/flowtests/FlowTest.java
@@ -33,6 +33,7 @@ import org.junit.Before;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
@@ -45,9 +46,12 @@ import com.ericsson.ei.App;
 import com.ericsson.ei.erqueryservice.ERQueryService;
 import com.ericsson.ei.erqueryservice.SearchOption;
 import com.ericsson.ei.handlers.UpStreamEventsHandler;
+import com.ericsson.ei.rmqhandler.RmqHandler;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.github.wnameless.json.flattener.JsonFlattener;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @TestExecutionListeners(listeners = { DependencyInjectionTestExecutionListener.class, FlowTest.class })
@@ -55,6 +59,7 @@ import com.fasterxml.jackson.databind.node.ObjectNode;
 public class FlowTest extends FlowTestBase {
 
     private static final String UPSTREAM_RESULT_FILE = "upStreamResultFile.json";
+    private static final String UPSTREAM_INPUT_FILE = "upStreamInput.json";
     private static final String RULES_FILE_PATH = "src/test/resources/ArtifactRules_new.json";
     private static final String EVENTS_FILE_PATH = "src/test/resources/test_events.json";
     private static final String AGGREGATED_OBJECT_FILE_PATH = "src/test/resources/AggregatedDocumentInternalCompositionLatest.json";
@@ -65,20 +70,39 @@ public class FlowTest extends FlowTestBase {
 
     @Mock
     private ERQueryService erQueryService;
+    
+    @Autowired
+    private RabbitTemplate rabbitMqTemplate;
 
     @Before
-    public void before() throws IOException {
-        MockitoAnnotations.initMocks(this);
-        upStreamEventsHandler.setEventRepositoryQueryService(erQueryService);
+    public void before() throws IOException {    	    	
+    	ObjectMapper objectMapper = new ObjectMapper();
+    	   
+    	if (!systemTest) {
+    		final URL upStreamResult = this.getClass().getClassLoader().getResource(UPSTREAM_RESULT_FILE);
+    		MockitoAnnotations.initMocks(this);
+    		upStreamEventsHandler.setEventRepositoryQueryService(erQueryService);
+    		    	
+    		ObjectNode objectNode = objectMapper.createObjectNode();
+    		JsonNode upstreamJson = objectMapper.readTree(upStreamResult);
+    		objectNode.set("upstreamLinkObjects", upstreamJson);
+    		objectNode.set("downstreamLinkObjects", objectMapper.createArrayNode());
 
-        final URL upStreamResult = this.getClass().getClassLoader().getResource(UPSTREAM_RESULT_FILE);
-        ObjectMapper objectMapper = new ObjectMapper();
-        ObjectNode objectNode = objectMapper.createObjectNode();
-        objectNode.set("upstreamLinkObjects", objectMapper.readTree(upStreamResult));
-        objectNode.set("downstreamLinkObjects", objectMapper.createArrayNode());
-
-        when(erQueryService.getEventStreamDataById(anyString(), any(SearchOption.class), anyInt(), anyInt(),
-                anyBoolean())).thenReturn(new ResponseEntity<>(objectNode, HttpStatus.OK));
+    		when(erQueryService.getEventStreamDataById(anyString(), any(SearchOption.class), anyInt(), anyInt(),
+    				anyBoolean())).thenReturn(new ResponseEntity<>(objectNode, HttpStatus.OK));
+    	} else {
+    		final URL upStreamInput = this.getClass().getClassLoader().getResource(UPSTREAM_INPUT_FILE);
+    		ArrayNode upstreamJson = (ArrayNode) objectMapper.readTree(upStreamInput);
+//    		rabbitMqTemplate.setQueue(null);
+    		if (upstreamJson != null) {
+    			for (JsonNode event : upstreamJson) {
+    				String eventStr = event.toString();
+    				rabbitMqTemplate.convertAndSend(eventStr);
+    			}    			
+    		}
+    		boolean wait = true;
+//    		upstreamJson.elem
+    	}
     }
 
     @Override

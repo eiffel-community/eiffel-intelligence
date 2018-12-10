5727150fe02b95ec24edbd93974422059376c1e1
Mohammad Saifullah <mohammad.saifullah@ericsson.com>
2018-12-10 15:34:56 +0100

    some reviewed changes

--- src/main/java/com/ericsson/ei/subscriptionhandler/SubscriptionValidator.java
+++ src/main/java/com/ericsson/ei/subscriptionhandler/SubscriptionValidator.java
@@ -38,141 +38,143 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class SubscriptionValidator {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(SubscriptionValidator.class);
-	private static final String SCHEMA_FILE_PATH = "/subscription_schema.json";
-    /**
-     * Validation of parameters values in subscriptions objects. Throws
-     * SubscriptionValidationException if validation of a parameter fails due to
-     * wrong format of parameter.
-     *
-     * @param subscription
-     */
-    public void validateSubscription(Subscription subscription) throws SubscriptionValidationException {
-        LOGGER.debug("Validation of subscription " + subscription.getSubscriptionName() + " Started.");
+	private static final Logger LOGGER = LoggerFactory.getLogger(SubscriptionValidator.class);
+	private static final String SCHEMA_FILE_PATH = "/schemas/subscription_schema.json";
+
+	/**
+	 * Validation of parameters values in subscriptions objects. Throws
+	 * SubscriptionValidationException if validation of a parameter fails due to
+	 * wrong format of parameter.
+	 *
+	 * @param subscription
+	 */
+	public void validateSubscription(Subscription subscription) throws SubscriptionValidationException {
+		LOGGER.debug("Validation of subscription " + subscription.getSubscriptionName() + " Started.");
 		this.validateSubscriptionName(subscription.getSubscriptionName());
-        this.validateNotificationMessageKeyValues(subscription.getNotificationMessageKeyValues(),
-                subscription.getRestPostBodyMediaType());
-        this.validateNotificationMeta(subscription.getNotificationMeta());
-        this.validateNotificationType(subscription.getNotificationType());
-        if (subscription.getNotificationType().equals("REST_POST")) {
-            this.RestPostMediaType(subscription.getRestPostBodyMediaType());
-        }
-        LOGGER.debug("Validating of subscription " + subscription.getSubscriptionName() + " finished successfully.");
-    }
+		this.validateNotificationMessageKeyValues(subscription.getNotificationMessageKeyValues(),
+				subscription.getRestPostBodyMediaType());
+		this.validateNotificationMeta(subscription.getNotificationMeta());
+		this.validateNotificationType(subscription.getNotificationType());
+		if (subscription.getNotificationType().equals("REST_POST")) {
+			this.RestPostMediaType(subscription.getRestPostBodyMediaType());
+		}
+		LOGGER.debug("Validating of subscription " + subscription.getSubscriptionName() + " finished successfully.");
+	}
 
-    /**
-     * Validation of subscriptionName parameter Throws
-     * SubscriptionValidationException if validation of the parameter fails due to
-     * wrong format of parameter.
-     *
-     * @param subscriptionName
-     */
-    private void validateSubscriptionName(String subscriptionName) throws SubscriptionValidationException {
-        String regex = "^[A-Za-z0-9_]+$";
-        if (subscriptionName == null) {
-            throw new SubscriptionValidationException("Required field SubscriptionName has not been set");
-        } else if (!Pattern.matches(regex, subscriptionName)) {
-            throw new SubscriptionValidationException("Wrong format of SubscriptionName: " + subscriptionName);
-        }
-    }
+	/**
+	 * Validation of subscriptionName parameter Throws
+	 * SubscriptionValidationException if validation of the parameter fails due to
+	 * wrong format of parameter.
+	 *
+	 * @param subscriptionName
+	 */
+	private void validateSubscriptionName(String subscriptionName) throws SubscriptionValidationException {
+		String regex = "^[A-Za-z0-9_]+$";
+		if (subscriptionName == null) {
+			throw new SubscriptionValidationException("Required field SubscriptionName has not been set");
+		} else if (!Pattern.matches(regex, subscriptionName)) {
+			throw new SubscriptionValidationException("Wrong format of SubscriptionName: " + subscriptionName);
+		}
+	}
 
-    /**
-     * Validation of NotificationMessageKeyValues parameters (key/values) Throws
-     * SubscriptionValidationException if validation of the parameter fails due to
-     * wrong format of parameter.
-     *
-     * @param notificationMessage
-     * @param restPostBodyMediaType
-     */
+	/**
+	 * Validation of NotificationMessageKeyValues parameters (key/values) Throws
+	 * SubscriptionValidationException if validation of the parameter fails due to
+	 * wrong format of parameter.
+	 *
+	 * @param notificationMessage
+	 * @param restPostBodyMediaType
+	 */
 
-    private void validateNotificationMessageKeyValues(List<NotificationMessageKeyValue> notificationMessage,
-                                                      String restPostBodyMediaType) throws SubscriptionValidationException {
-        for (NotificationMessageKeyValue item : notificationMessage) {
-            String testKey = item.getFormkey();
-            String testValue = item.getFormvalue();
-            if (restPostBodyMediaType != null
-                    && restPostBodyMediaType.equals(MediaType.APPLICATION_FORM_URLENCODED.toString())) { // FORM/POST
-                // PARAMETERS
-                if (StringUtils.isBlank(testKey) || StringUtils.isBlank(testValue)) {
-                    throw new SubscriptionValidationException(
-                            "Value & Key  in notificationMessage must have a values: " + notificationMessage);
-                }
-            } else {
-                if (notificationMessage.size() != 1) {
-                    throw new SubscriptionValidationException(
-                            "Only one array is allowed for notificationMessage when NOT using key/value pairs: "
-                                    + notificationMessage);
-                } else if (testKey != null && !testKey.isEmpty()) {
-                    throw new SubscriptionValidationException(
-                            "Key in notificationMessage must be empty when NOT using key/value pairs: "
-                                    + notificationMessage);
-                } else if (StringUtils.isBlank(testValue)) {
-                    throw new SubscriptionValidationException(
-                            "Value in notificationMessage must have a value when NOT using key/value pairs: "
-                                    + notificationMessage);
-                }
-            }
-        }
-    }
+	private void validateNotificationMessageKeyValues(List<NotificationMessageKeyValue> notificationMessage,
+			String restPostBodyMediaType) throws SubscriptionValidationException {
+		for (NotificationMessageKeyValue item : notificationMessage) {
+			String testKey = item.getFormkey();
+			String testValue = item.getFormvalue();
+			if (restPostBodyMediaType != null
+					&& restPostBodyMediaType.equals(MediaType.APPLICATION_FORM_URLENCODED.toString())) { // FORM/POST
+				// PARAMETERS
+				if (StringUtils.isBlank(testKey) || StringUtils.isBlank(testValue)) {
+					throw new SubscriptionValidationException(
+							"Value & Key  in notificationMessage must have a values: " + notificationMessage);
+				}
+			} else {
+				if (notificationMessage.size() != 1) {
+					throw new SubscriptionValidationException(
+							"Only one array is allowed for notificationMessage when NOT using key/value pairs: "
+									+ notificationMessage);
+				} else if (testKey != null && !testKey.isEmpty()) {
+					throw new SubscriptionValidationException(
+							"Key in notificationMessage must be empty when NOT using key/value pairs: "
+									+ notificationMessage);
+				} else if (StringUtils.isBlank(testValue)) {
+					throw new SubscriptionValidationException(
+							"Value in notificationMessage must have a value when NOT using key/value pairs: "
+									+ notificationMessage);
+				}
+			}
+		}
+	}
 
-    /**
-     * Validation of notificationMeta parameter Throws
-     * SubscriptionValidationException if validation of the parameter fails due to
-     * wrong format of parameter.
-     *
-     * @param notificationMeta
-     */
-    private void validateNotificationMeta(String notificationMeta) throws SubscriptionValidationException {
-        String regex = ".*[\\s].*";
-        if (notificationMeta == null) {
-            throw new SubscriptionValidationException("Required field NotificationMeta has not been set");
-        } else if (Pattern.matches(regex, notificationMeta)) {
-            throw new SubscriptionValidationException("Wrong format of NotificationMeta: " + notificationMeta);
-        }
-    }
+	/**
+	 * Validation of notificationMeta parameter Throws
+	 * SubscriptionValidationException if validation of the parameter fails due to
+	 * wrong format of parameter.
+	 *
+	 * @param notificationMeta
+	 */
+	private void validateNotificationMeta(String notificationMeta) throws SubscriptionValidationException {
+		String regex = ".*[\\s].*";
+		if (notificationMeta == null) {
+			throw new SubscriptionValidationException("Required field NotificationMeta has not been set");
+		} else if (Pattern.matches(regex, notificationMeta)) {
+			throw new SubscriptionValidationException("Wrong format of NotificationMeta: " + notificationMeta);
+		}
+	}
 
-    /**
-     * Validation of notificationType parameter Throws
-     * SubscriptionValidationException if validation of the parameter fails due to
-     * wrong format of parameter.
-     *
-     * @param notificationType
-     */
-    private void validateNotificationType(String notificationType) throws SubscriptionValidationException {
-        String regexMail = "[\\s]*MAIL[\\\\s]*";
-        String regexRestPost = "[\\s]*REST_POST[\\\\s]*";
-        if (notificationType == null) {
-            throw new SubscriptionValidationException("Required field NotificationType has not been set");
-        } else if (!(Pattern.matches(regexMail, notificationType) || Pattern.matches(regexRestPost, notificationType))) {
-            throw new SubscriptionValidationException("Wrong format of NotificationType: " + notificationType);
-        }
-    }
+	/**
+	 * Validation of notificationType parameter Throws
+	 * SubscriptionValidationException if validation of the parameter fails due to
+	 * wrong format of parameter.
+	 *
+	 * @param notificationType
+	 */
+	private void validateNotificationType(String notificationType) throws SubscriptionValidationException {
+		String regexMail = "[\\s]*MAIL[\\\\s]*";
+		String regexRestPost = "[\\s]*REST_POST[\\\\s]*";
+		if (notificationType == null) {
+			throw new SubscriptionValidationException("Required field NotificationType has not been set");
+		} else if (!(Pattern.matches(regexMail, notificationType)
+				|| Pattern.matches(regexRestPost, notificationType))) {
+			throw new SubscriptionValidationException("Wrong format of NotificationType: " + notificationType);
+		}
+	}
 
-    private void RestPostMediaType(String restPostMediaType) throws SubscriptionValidationException {
-        String regexApplication_JSON = "[\\s]*application/json[\\\\s]*";
-        String regexApplicationFormUrlEncoded = "[\\s]*application/x-www-form-urlencoded[\\\\s]*";
-        if (restPostMediaType == null) {
-            throw new SubscriptionValidationException("Required field RestPostMediaType has not been set");
-        } else if (!(Pattern.matches(regexApplication_JSON, restPostMediaType)
-                || Pattern.matches(regexApplicationFormUrlEncoded, restPostMediaType))) {
-            throw new SubscriptionValidationException("Wrong format of RestPostMediaType: " + restPostMediaType);
-        }
-    }
+	private void RestPostMediaType(String restPostMediaType) throws SubscriptionValidationException {
+		String regexApplication_JSON = "[\\s]*application/json[\\\\s]*";
+		String regexApplicationFormUrlEncoded = "[\\s]*application/x-www-form-urlencoded[\\\\s]*";
+		if (restPostMediaType == null) {
+			throw new SubscriptionValidationException("Required field RestPostMediaType has not been set");
+		} else if (!(Pattern.matches(regexApplication_JSON, restPostMediaType)
+				|| Pattern.matches(regexApplicationFormUrlEncoded, restPostMediaType))) {
+			throw new SubscriptionValidationException("Wrong format of RestPostMediaType: " + restPostMediaType);
+		}
+	}
 
-    /**
-     * Validation of email address Throws SubscriptionValidationException if
-     * validation of the parameter fails due to wrong format of parameter.
-     *
-     * @param email
-     */
-    public void validateEmail(String email) throws SubscriptionValidationException {
-        final Pattern VALID_EMAIL_ADDRESS_REGEX = Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$",
-                Pattern.CASE_INSENSITIVE);
-        Matcher matcher = VALID_EMAIL_ADDRESS_REGEX.matcher(email);
-        if (!(matcher.matches())) {
-            throw new SubscriptionValidationException("Wrong email address: " + email);
-        }
-    }
+	/**
+	 * Validation of email address Throws SubscriptionValidationException if
+	 * validation of the parameter fails due to wrong format of parameter.
+	 *
+	 * @param email
+	 */
+	public void validateEmail(String email) throws SubscriptionValidationException {
+		final Pattern VALID_EMAIL_ADDRESS_REGEX = Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$",
+				Pattern.CASE_INSENSITIVE);
+		Matcher matcher = VALID_EMAIL_ADDRESS_REGEX.matcher(email);
+		if (!(matcher.matches())) {
+			throw new SubscriptionValidationException("Wrong email address: " + email);
+		}
+	}
 
 	public void validateWithSchema(Subscription subscription) throws SubscriptionValidationException {
 		LOGGER.debug("Validation of subscription " + subscription.getSubscriptionName() + " Started.");
@@ -188,6 +190,7 @@ public class SubscriptionValidator {
 		JSONObject jsonSubscriptionObj = new JSONObject(subscriptionJson);
 		JSONObject jsonSchemaObj = new JSONObject(
 				new JSONTokener(this.getClass().getResourceAsStream(SCHEMA_FILE_PATH)));
+
 		// JSONObject jsonSchema = new JSONObject(new JSONTokener(file));
 		Schema schema = SchemaLoader.load(jsonSchemaObj);
 		try {

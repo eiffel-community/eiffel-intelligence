e74975bb6c51e021a9639e69b5cc830e7e8b4951
Tobias Akervik <tobias.akervik@ericsson.com>
2019-01-24 09:51:53 +0100

    Dockerfile for building images for Docker and Kubernetes

---
+++ src/main/docker/start-service.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# These two varibales need to be set from Docker-compose or K8S at startup if MB and/or DB healthcheck should be used.
+#WAIT_MB_HOSTS="localhost:15672 localhost:15672"
+
+if [ ! -z "$WAIT_MB_HOSTS" ]
+then
+  /eiffel/health-check.sh "$WAIT_MB_HOSTS"
+fi
+
+if [ ! -z "$WAIT_DB_HOSTS" ]
+then
+  /eiffel/health-check.sh "$WAIT_DB_HOSTS"
+fi
+
+
+echo
+echo "Starting Eiffel-Intelligence"
+echo
+
+/eiffel/health-check.sh && catalina.sh run

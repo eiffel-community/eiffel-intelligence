d01c57dd8276476d66f25ce4937e102b1e0c4109
Mohammad Saifullah <mohammad.saifullah@ericsson.com>
2019-01-26 15:48:00 +0100

    minor

--- src/test/java/com/ericsson/ei/subscriptionhandler/test/SubscriptionHandlerTest.java
+++ src/test/java/com/ericsson/ei/subscriptionhandler/test/SubscriptionHandlerTest.java
@@ -292,7 +292,9 @@ public class SubscriptionHandlerTest {
                 .perform(MockMvcRequestBuilders.get(missedNotificationUrl).param("SubscriptionName", subscriptionName))
                 .andReturn();
         String response = result.getResponse().getContentAsString().replace("\\", "");
-        assertEquals("{\"queryResponseEntity\":" + input.toString().replace("\\", "") + "}", response);
+        assertEquals(
+                "{\"queryResponseEntity\":{\"missedNotification\":{ \"[" + input.toString().replace("\\", "") + "\"}",
+                response);
         assertEquals(HttpStatus.OK.value(), result.getResponse().getStatus());
     }
 

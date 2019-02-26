4628e6b60b654d4acca4b6cd2049046be1da3049
Christoffer Cortes Sjöwall <christoffer.cortes.sjowall@ericsson.com>
2019-02-26 13:29:40 +0100

    Fix Qpid start up and settings

--- src/functionaltests/java/com/ericsson/ei/utils/AMQPBrokerManager.java
+++ src/functionaltests/java/com/ericsson/ei/utils/AMQPBrokerManager.java
@@ -1,55 +1,54 @@
 package com.ericsson.ei.utils;
 
-import com.google.common.io.Files;
-
-import java.net.URL;
+import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
 
 import org.apache.qpid.server.SystemLauncher;
-
+import org.apache.qpid.server.model.SystemConfig;
+import org.apache.qpid.server.store.MemoryMessageStore;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class AMQPBrokerManager {
-    public boolean isRunning = false;
-    
-    private final SystemLauncher systemLauncher = new SystemLauncher();
-    
     private String path;
-    private String port;
+    private int port;
+    public boolean isRunning = false;
+    private String passwordFile = "src/functionaltests/resources/configs/password.properties";
 
-    public AMQPBrokerManager(String path, String port) {
+    private SystemLauncher systemLauncher = new SystemLauncher();
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(TestConfigs.class);
+
+    public AMQPBrokerManager(String path, int port) {
         super();
         this.path = path;
         this.port = port;
     }
 
     private Map<String, Object> createSystemConfig() {
+        String passwordPath = new File(passwordFile).getAbsolutePath();
+        Map<String, String> context = new HashMap<>();
+        context.put("qpid.amqp_port", "" + port);
+        context.put("qpid.pass_file", passwordPath);
+
         Map<String, Object> attributes = new HashMap<>();
-        URL initialConfig = AMQPBrokerManager.class.getClassLoader().getResource(path);
-        attributes.put("type", "Memory");
-        attributes.put("qpid.amqp_port", "" + port);
-        attributes.put("qpid.pass_file", "src/test/resources/configs/password.properties");
-        attributes.put("qpid.work_dir", Files.createTempDir().getAbsolutePath());
-        attributes.put("initialConfigurationLocation", initialConfig.toExternalForm());
-        attributes.put("startupLoggedToSystemOut", true);
+        attributes.put(SystemConfig.CONTEXT, context);
+        attributes.put(SystemConfig.TYPE, MemoryMessageStore.TYPE);
+        attributes.put(SystemConfig.INITIAL_CONFIGURATION_LOCATION, path);
         return attributes;
     }
-    
-    
-    public void startBroker() throws Exception {
 
+    public void startBroker() throws Exception {
         try {
             systemLauncher.startup(createSystemConfig());
         } catch (Exception e) {
-            System.out.println("ERROR: Create QPID System Config fail. Error: " + e.getMessage());
+            LOGGER.error("Create QPID System Config fail. \nError: {}", e.getMessage());
             e.printStackTrace();
-        } finally {
-            systemLauncher.shutdown();
         }
         isRunning = true;
     }
 
-    
     public void stopBroker() {
         systemLauncher.shutdown();
         isRunning = false;

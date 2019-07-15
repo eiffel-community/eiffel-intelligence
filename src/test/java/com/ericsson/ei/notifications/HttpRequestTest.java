ff432b588587c4a20dbd91481d0feb0e6552920e
Anders Breid <anders.breid@ericsson.com>
2019-07-15 09:04:17 +0200

    Add tests for HttpRequest

--- src/test/java/com/ericsson/ei/notifications/HttpRequestTest.java
+++ src/test/java/com/ericsson/ei/notifications/HttpRequestTest.java
@@ -13,6 +13,7 @@
 */
 package com.ericsson.ei.notifications;
 
+import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.when;
 import java.io.File;
 import java.io.IOException;
@@ -57,7 +58,7 @@ public class HttpRequestTest {
     private static final String USERNAME = "username";
     private static final String PASSWORD = "password";
 
-    private String encoding = Base64.getEncoder().encodeToString((USERNAME + ":" + PASSWORD).getBytes());
+    private String encoding;
 
     private RestPostSubscriptionObject subscription;
     private static MongodForTestsFactory testsFactory;
@@ -92,8 +93,8 @@ public class HttpRequestTest {
     public void beforeTests() throws IOException {
         subscription = new RestPostSubscriptionObject("My_subscription_name");
         subscription.setAuthenticationType("BASIC_AUTH_JENKINS_CSRF")
-                    .setUsername("user")
-                    .setPassword("pass")
+                    .setUsername(USERNAME)
+                    .setPassword(PASSWORD)
                     .setNotificationMeta(TEST_URL)
                     .setRestPostBodyMediaType(MEDIA_TYPE);
 
@@ -104,15 +105,56 @@ public class HttpRequestTest {
                    .setSubscriptionJson(subscriptionJsonNode)
                    .setMapNotificationMessage(mapNotificationMessage)
                    .setUrl(TEST_URL);
+
+        encoding = Base64.getEncoder().encodeToString((USERNAME + ":" + PASSWORD).getBytes());
     }
 
+    /**
+     * Test the complete process by giving a crumb key, and a crumb value an ensure those are added
+     * correctly to the headers, this should confirm that all steps in the headers preparation is
+     * completed.
+     *
+     * @throws Exception
+     */
     @Test
-    public void buildTest() throws Exception {
-        String crumbString = "{\"crumbRequestField\": \"my_crumb_key\", \"crumb\": \"my_crumb\"}";
-        JsonNode crumbNode = mapper.readTree(crumbString);
+    public void testAuthentication() throws Exception {
+        String crumb = "my_crumb";
+        String crumbKey = "my_crumb_key";
+        String crumbResponse = "{\"crumbRequestField\": \"" + crumbKey + "\", \"crumb\": \"" + crumb
+                + "\"}";
+        JsonNode crumbResponseNode = mapper.readTree(crumbResponse);
 
-        when(jenkinsCrumb.fetchJenkinsCrumb(Mockito.any(), Mockito.any())).thenReturn(crumbNode);
+        // Here we ensure encoding is correct, and URL is correct or crumb will become null.
+        when(jenkinsCrumb.fetchJenkinsCrumb(encoding, TEST_URL)).thenReturn(
+                crumbResponseNode);
+
+        // Build the request.
         httpRequest.build();
+
+        // crom the request extract the processed crumb and remove '[]'
+        String processedCrumb = httpRequest.getRequest()
+                                           .getHeaders()
+                                           .get(crumbKey)
+                                           .toString()
+                                           .replace("[", "")
+                                           .replace("]", "");
+
+        assertEquals("The expected crumb vs the crumb in headers: ", crumb, processedCrumb);
+    }
+
+    /**
+     * Test to ensure that the correct content type is set in the request.
+     *
+     * @throws Exception
+     */
+    @Test
+    public void testCorrectMediaTypeInRequest() throws Exception {
+        // Build the request.
+        httpRequest.build();
+
+        String contentType = httpRequest.getRequest().getHeaders().getContentType().toString();
+        assertEquals("Content type is equal to content type in subscription", MEDIA_TYPE,
+                contentType);
     }
 
 }

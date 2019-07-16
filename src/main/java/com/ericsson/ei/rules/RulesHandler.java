d2b413789a9a46377972ae9df31849f158966fce
Christoffer Cortes Sjöwall <35095827+Christoffer-Cortes@users.noreply.github.com>
2019-07-16 10:06:59 +0200

    Add GET /rules endpoint (#300)
    * /rules is now an endpoint that fetches the active rules content and path

--- src/main/java/com/ericsson/ei/rules/RulesHandler.java
+++ src/main/java/com/ericsson/ei/rules/RulesHandler.java
@@ -104,6 +104,15 @@ public class RulesHandler {
     }
 
     /**
+     * Returns the active rules used by Eiffel Intelligence.
+     *
+     * @return rules content
+     */
+    public JsonNode getRulesContent() {
+        return parsedJson;
+    }
+
+    /**
      * Reads the content of a given rules file path or URL.
      *
      * @return the rules file content

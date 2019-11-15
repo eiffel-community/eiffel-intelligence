aa57bfbf187ad122f90b1c3241170f5d5065233f
Anders Breid <anders.breid@ericsson.com>
2019-11-15 10:50:52 +0100

    Add coopyright and comments, move private method below public

--- src/main/java/com/ericsson/ei/utils/SafeLdapServer.java
+++ src/main/java/com/ericsson/ei/utils/SafeLdapServer.java
@@ -1,15 +1,36 @@
+/*
+   Copyright 2019 Ericsson AB.
+   For a full list of individual contributors, please see the commit history.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
 package com.ericsson.ei.utils;
 
 import org.apache.commons.lang3.StringUtils;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+/**
+ * This class should ensure the safety of an LDAP server list by creating a new JSONArray where the
+ * objects inside does not have the key and value for password.
+ *
+ */
 public class SafeLdapServer {
 
     /**
-     * By creating our own LDAP setting object to display we have control of what values are
-     * shown to the user. This also makes it impossible to slip unwanted values by mistake if
-     * the user configure the property with misspelled keys.
+     * By creating our own LDAP setting object to display we have control of what values are shown
+     * to the user. This also makes it impossible to slip unwanted values by mistake if the user
+     * configure the property with misspelled keys.
      *
      * @param String :
      * @return JSONArray
--- src/test/java/com/ericsson/ei/utils/SafeLdapServerTest.java
+++ src/test/java/com/ericsson/ei/utils/SafeLdapServerTest.java
@@ -1,10 +1,13 @@
 /*
    Copyright 2019 Ericsson AB.
    For a full list of individual contributors, please see the commit history.
+
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
    You may obtain a copy of the License at
+
        http://www.apache.org/licenses/LICENSE-2.0
+
    Unless required by applicable law or agreed to in writing, software
    distributed under the License is distributed on an "AS IS" BASIS,
    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -63,17 +66,6 @@ public class SafeLdapServerTest {
         assertSafeLdapServerList(serverList);
     }
 
-    private void assertSafeLdapServerList(JSONArray serverList) {
-        serverList.forEach(item -> {
-            JSONObject ldapServer = (JSONObject) item;
-            Set<String> keys = ldapServer.keySet();
-            assertFalse("Safe LDAP object should not contain key password",
-                    keys.contains(PASSWORD_KEY));
-            assertTrue("Safe LDAP object should contain key url", keys.contains(URL_KEY));
-            assertEquals("Safe LDAP object url value", URL_VALUE, ldapServer.get(URL_KEY));
-        });
-    }
-
     @Test
     public void testInputValueNull() throws Exception {
         final JSONArray serverList = SafeLdapServer.createLdapSettingsArray("");
@@ -86,4 +78,14 @@ public class SafeLdapServerTest {
         assertEquals("Safe LDAP server list should be empty", 0, serverList.length());
     }
 
+    private void assertSafeLdapServerList(JSONArray serverList) {
+        serverList.forEach(item -> {
+            JSONObject ldapServer = (JSONObject) item;
+            Set<String> keys = ldapServer.keySet();
+            assertFalse("Safe LDAP object should not contain key password",
+                    keys.contains(PASSWORD_KEY));
+            assertTrue("Safe LDAP object should contain key url", keys.contains(URL_KEY));
+            assertEquals("Safe LDAP object url value", URL_VALUE, ldapServer.get(URL_KEY));
+        });
+    }
 }

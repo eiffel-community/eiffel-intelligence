361cd1e3520be1b139fc270b9914f9638fae8755
Anders Breid <anders.breid@ericsson.com>
2019-11-25 13:14:36 +0100

    Fixed comments

--- src/main/java/com/ericsson/ei/mongo/MongoUri.java
+++ src/main/java/com/ericsson/ei/mongo/MongoUri.java
@@ -16,6 +16,8 @@
 */
 package com.ericsson.ei.mongo;
 
+import static org.junit.Assert.assertFalse;
+
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -26,7 +28,8 @@ public class MongoUri {
     private static final String HIDDEN_PASSWORD = "********";
 
     /**
-     * This method replaces the given password if any.
+     * The input uri will have it´s password replaced wit stars and returned uri is validated not to
+     * contain the password.
      *
      * Example input uri: mongodb://username:password@hostname1:27017,hostname2:27017
      *
@@ -42,13 +45,18 @@ public class MongoUri {
 
         String oldPassword = extractPasswordFromUri(uri);
         String modifiedUri = replacePassword(uri, oldPassword, HIDDEN_PASSWORD);
+
+        final String oldPasswordField = String.format(":%s", oldPassword);
+        assertFalse("URI contains old password.", modifiedUri.contains(oldPasswordField));
+
         return modifiedUri;
     }
 
     /**
-     * This method replaces the given password if any.
+     * Raplace existing password in uri with new password.
      *
-     * Example input uri: mongodb://username:password@hostname1:27017,hostname2:27017 input new_password
+     * Example input uri: mongodb://username:password@hostname1:27017,hostname2:27017 input
+     * new_password
      *
      * Returns mongodb://username:new_password@hostname1:27017,hostname2:27017
      *
@@ -67,8 +75,6 @@ public class MongoUri {
     }
 
     /**
-     * Returns the password if any exist in a URI
-     *
      * Example input uri: mongodb://username:mypasswd@hostname1:27017,hostname2:27017
      *
      * Returns mypasswd
@@ -77,7 +83,7 @@ public class MongoUri {
      * @return
      */
     public static String extractPasswordFromUri(String uri) {
-        String secret = "";
+        String password = "";
         /**
          * Matcher that match string between first : and @.
          *
@@ -93,16 +99,16 @@ public class MongoUri {
             String authenticationDetails = matcher.group(0).replace("//", "");
             if (authenticationDetails.contains(":")) {
                 String[] authenticationDetailsList = authenticationDetails.split(":");
-                secret = authenticationDetailsList[1];
+                password = authenticationDetailsList[1];
             }
         }
-        return secret;
+        return password;
     }
 
-
     /**
-     * By ensuring password is behoind : we know we do not replace username in case username and
-     * password is the same.
+     * The password will always have ':' in front. Username has ':' after. By replacing (':' +
+     * password) we know we don´t replace username if username and password is the same, like
+     * /admin:admin@*.
      *
      * @param uri
      * @param oldPassword
@@ -110,7 +116,9 @@ public class MongoUri {
      * @return
      */
     private static String replacePassword(String uri, String oldPassword, String newPassword) {
-        return uri.replaceFirst(String.format(":%s", oldPassword),
-                String.format(":%s", newPassword));
+        final String oldPasswordField = String.format(":%s", oldPassword);
+        final String newPasswordField = String.format(":%s", newPassword);
+
+        return uri.replaceFirst(oldPasswordField, newPasswordField);
     }
 }

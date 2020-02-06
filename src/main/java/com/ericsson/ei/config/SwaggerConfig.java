06bd1efacf160cc119af06c41695d3a2b942ee07
e-pettersson-ericsson <38855976+e-pettersson-ericsson@users.noreply.github.com>
2020-02-06 07:58:29 +0100

    Remove deprecated methods/annotations in SwaggerConfig and Travis file (#435)
    * Switch from deprecated method in Swagger ApiInfo
    * Remove deprecated variables in travis file
    * Add contact information in Swagger UI

--- src/main/java/com/ericsson/ei/config/SwaggerConfig.java
+++ src/main/java/com/ericsson/ei/config/SwaggerConfig.java
@@ -25,6 +25,7 @@ import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -33,11 +34,15 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class SwaggerConfig {
 
+    private static final String CONTACT_NAME = "Eiffel Intelligence Maintainers";
+    private static final String CONTACT_URL = "https://github.com/eiffel-community/eiffel-intelligence";
+    private static final String CONTACT_EMAIL = "eiffel-community@googlegroups.com";
+
     @Autowired
     ParseInstanceInfoEI parseInstanceInfoEI;
 
     @Bean
-    public Docket productApi() {
+    public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.ericsson.ei.controller"))
@@ -45,13 +50,14 @@ public class SwaggerConfig {
                 .build()
                 .apiInfo(metaData());
     }
-    @SuppressWarnings("deprecation")
+
     private ApiInfo metaData() {
         ApiInfo apiInfo = new ApiInfo(
                 "Eiffel Intelligence REST API",
                 "A real time data aggregation and analysis solution for Eiffel events.",
                 parseInstanceInfoEI.getVersion(),
-                "Terms of service","",
+                "Terms of service",
+                new Contact(CONTACT_NAME, CONTACT_URL, CONTACT_EMAIL),
                "Apache License Version 2.0",
                "https://www.apache.org/licenses/LICENSE-2.0");
         return apiInfo;

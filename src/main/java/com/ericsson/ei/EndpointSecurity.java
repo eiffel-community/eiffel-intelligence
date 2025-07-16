3c5e600698eb7316302b85610d01cc2f9de1179a
Roman Szturc <roman.szturc.ext@ericsson.com>
2025-07-16 12:30:29 +0200

    CSRF kept

--- src/main/java/com/ericsson/ei/EndpointSecurity.java
+++ src/main/java/com/ericsson/ei/EndpointSecurity.java
@@ -145,6 +145,12 @@ public class EndpointSecurity extends WebSecurityConfigurerAdapter {
 
     private void disableCSRF(HttpSecurity http) throws Exception {
         http.csrf()
+            // The application uses non-browser clients. Yes, there is swagger interface,
+            // but is's used only for testing/tuning.
+            //
+            // From https://docs.spring.io/spring-security/reference/features/exploits/csrf.html
+            // "If you are creating a service that is used only by non-browser clients,
+            //  you likely want to disable CSRF protection."
             .disable();
     }
 
